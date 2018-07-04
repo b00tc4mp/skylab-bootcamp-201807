d157024b5f6ca987e5cc4addab5e788cb9205324
Maider <info@maiderhernandorena.com>
2018-07-04 09:50:16 +0200

    add changes project2

--- staff/maider-hernandorena/pre-course/tema2/project2.js
+++ staff/maider-hernandorena/pre-course/tema2/project2.js
@@ -1,15 +1,15 @@
 // This is a example of array of objects... each position of array contains one object...
 var flights = [
-{id: 00, to: "New York", from: "Barcelona", cost: 700,scale: false},
-{id: 01, to: "Los Angeles", from: "Madrid", cost: 1100,scale: true},
-{id: 02, to: "Paris", from: "Barcelona", cost: 210,scale: false},
-{id: 03, to: "Roma", from: "Barcelona", cost: 150,scale: false},
-{id: 04, to: "London", from: "Madrid", cost: 200,scale: false},
-{id: 05, to: "Madrid", from: "Barcelona", cost: 90,scale: false},
-{id: 06, to: "Tokyo", from: "Madrid", cost: 1500,scale: true},
-{id: 07, to: "Shangai", from: "Barcelona", cost: 800,scale: true},
-{id: 08, to: "Sydney", from: "Barcelona", cost: 150,scale: true},
-{id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150,scale: false}
+	{id: 00, to: "New York", from: "Barcelona", cost: 700,scale: false},
+	{id: 01, to: "Los Angeles", from: "Madrid", cost: 1100,scale: true},
+	{id: 02, to: "Paris", from: "Barcelona", cost: 210,scale: false},
+	{id: 03, to: "Roma", from: "Barcelona", cost: 150,scale: false},
+	{id: 04, to: "London", from: "Madrid", cost: 200,scale: false},
+	{id: 05, to: "Madrid", from: "Barcelona", cost: 90,scale: false},
+	{id: 06, to: "Tokyo", from: "Madrid", cost: 1500,scale: true},
+	{id: 07, to: "Shangai", from: "Barcelona", cost: 800,scale: true},
+	{id: 08, to: "Sydney", from: "Barcelona", cost: 150,scale: true},
+	{id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150,scale: false}
 ]
 console.log(flights[0].to) //output: New York
 
@@ -50,14 +50,14 @@ console.log (avg);
 
 //También podrá ver cuantos vuelos efectúan escalas.
 function scaleCounter() {
-			var counter = 0;
-			flights.forEach(function(obj){
-				if (obj.scale === true) {
-					counter++;
-				}
-			})
-			console.log("Hay " + counter + " vuelos con escalas");
+	var counter = 0;
+	flights.forEach(function(obj){
+		if (obj.scale === true) {
+			counter++;
 		}
+	})
+	console.log("Hay " + counter + " vuelos con escalas");
+}
 console.log(scaleCounter());
 
 //Y, sabiendo que los ultimos 5 vuelos (los últimos 5 ID's) son los últimos del día, muestra al usuario sus destinos.
