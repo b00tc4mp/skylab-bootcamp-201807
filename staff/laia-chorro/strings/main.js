bf7d17c2a5f7db13cc1186164c7dfcf1bf75c42c
laia89 <laiacy234@gmail.com>
2018-07-09 17:45:24 +0200

    write string exercices down

---
+++ staff/laia-chorro/strings/main.js
@@ -0,0 +1,35 @@
+// count words
+
+function countWords(string) {
+    // TODO: count words in string using a standard loop
+
+    var words = 0,
+        string = string.trim();
+    for (var i = 0; i < string.length; i++) {
+        var letter = string[i],
+            nextLetter = string[i+1],
+            space = ' ' || ' ';
+
+        
+
+        if (letter === space) {
+            words++;
+        }
+        
+    }
+debugger;
+
+    return words;
+}
+
+console.log(countWords('hello world') === 2); // => true
+
+console.log(countWords('') === 0); // => true
+
+console.log(countWords('1 2 3 4 5') === 5); // => true
+
+console.log(countWords('one two  three          four     five  ') === 5); // => true
+
+// Ex2: find words (that match expression in provided function)
+
+
