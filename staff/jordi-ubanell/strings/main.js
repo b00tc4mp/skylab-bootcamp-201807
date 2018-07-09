bc5fe8374895738d92bc9d5d759c33b6588867d7
Jordi Ubanell <jordi@jordiubanell.com>
2018-07-09 18:16:38 +0200

    strings

---
+++ staff/jordi-ubanell/strings/main.js
@@ -0,0 +1,30 @@
+// COUNT WORDS
+
+function countWords(string){
+    // TODO: count words in string using a standard loop ()split and trim are forbidden, taking into account spaces and tabs
+   for (var i = 0; i<string; i++){
+    if 
+   }
+}
+
+
+
+console.log(countWords('hello world') === 2); // => true
+
+console.log(countWords('') === 0); // => true
+
+console.log (countWords('1 2 3 4 5') === 5); // => true
+
+console.log (countWords(' ') === 0); // => true
+
+console.log (countWords ('one  twho      tree four   five') === 5); // => true
+
+// split to words
+
+function splitTowords(string){
+        // TODO implment using standard loop
+}
+
+var words = splitTowords('jhello world');
+
+console.log(words.lenght === )
