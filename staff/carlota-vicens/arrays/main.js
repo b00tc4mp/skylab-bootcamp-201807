1d222cb04bb051f4dde1dcbb5d0dcf480618d204
Carlota <krlota.barna@gmail.com>
2018-07-09 17:09:12 +0200

    array exemple

---
+++ staff/carlota-vicens/arrays/main.js
@@ -0,0 +1,13 @@
+//add all elements
+
+//console.log(addElements([1,2,3,4,5])===15); // true
+//console.log(addElements([1,2,3,4,5,6])===21); // true
+//console.log(addElements([1,2,3])===6); // true
+
+function addElements(array){
+    res=0;
+    for (var i=0; i<array.length; i++ ){
+        res +=array[i];
+    }
+    return res;
+}
