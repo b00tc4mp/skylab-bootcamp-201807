ab3d5e5685254aca4d921666f284c101d7f0a264
JuanSensio <juansensio03@gmail.com>
2018-07-10 16:46:25 +0200

    strings update

--- staff/juan-sensio/strings/scripts/count-words-test.js
+++
@@ -1,7 +0,0 @@
-console.log(countWords('hello world') === 2); // true
-console.log(countWords('') === 0); // true
-console.log(countWords('1 2 3 4 5') === 5); // true
-console.log(countWords('    ') === 0); // true
-console.log(countWords('one   two       three   four     five') === 5); // true
-console.log(countWords('hola mundo \t\n') === 2); // true
-console.log(countWords('... ,,, ;;; :::') === 0); // true
