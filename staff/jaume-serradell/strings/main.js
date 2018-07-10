6bb69e7976e72b3732ada00b144722ec2840813b
jaumeserr <jaumeserr@gmail.com>
2018-07-10 13:14:34 +0200

    add string exercices

--- staff/jaume-serradell/strings/main.js
+++ staff/jaume-serradell/strings/main.js
@@ -2,22 +2,18 @@
 
 function countWords(string) {
     // TODO: count words in string using a standard loop (split and trim are forbidden), taking into account spaces and tabs
-    var words = 0;
     
-    for (var i=0; i<string.length; i++) {
-        if(string === ' ') {
-            words++;
-        }
-    }
 
-    console.log(words);
 }
 
-//console.log(countWords('hello world') === 2); // => true
+console.log(countWords('hello world'));
+console.log(countWords('hello world') === 2); // => true
 console.log(countWords('') === 0); // => true
 console.log(countWords('1 2 3 4 5') === 5); // => true
 console.log(countWords('    ') === 0); // => true
 console.log(countWords('one   two       three   four     five') === 5); // => true
+console.log(countWords('hola mundo \t\n') === 2); // => true
+console.log(countWords('... ,,, ;;; :::') === 0); // => true
 
 
 // split to words
