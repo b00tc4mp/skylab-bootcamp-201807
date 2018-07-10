4cb9eb5b4bc86c3f520520e0135f2dbd82e6a99a
Maider <info@maiderhernandorena.com>
2018-07-10 17:39:21 +0200

    strings changes

--- staff/maider-hernandorena/strings/scripts/split-to-words.js
+++ staff/maider-hernandorena/strings/scripts/split-to-words.js
@@ -1,19 +1,43 @@
 // split to words
-function splitToWords(string) {
-    var space = " ",
+function _splitToWords(string) {
         word = "",
         wordsArr = [];
 
     for (var i = 0; i < string.length; i++) {
-        var letter = string[i],
-            nextLetter = string[i+1];
-        if (letter !== space && !hasSymbol(letter)) {
+        var letter = string[i];
+        //var nextLetter = string[i+1];
+        if (!isBlank(letter) && !hasSymbol(letter)) {
             word += letter;
-            if (!nextLetter || nextLetter === space) {
+            /* if (!nextLetter || nextLetter === isBlank(letter)) {
                 wordsArr.push(word);
                 word = "";
-            }
+            } */
         } 
+        if (word.length) wordsArr.push(word);
     }
     return wordsArr;
+}
+
+// split to words CLASE
+
+function splitToWords(string) {
+    var words = [];
+    var currentWord = '';
+
+    for (var i = 0; i < string.length; i++) {
+        var char = string[i];
+
+        if (!isBlank(char) && !hasSymbol(char)) {
+            //currentWord += char;
+            currentWord = currentWord.concat(char);
+        } else if (currentWord.length) {
+            words.push(currentWord);
+
+            currentWord = '';
+        } 
+    }
+
+    if (currentWord.length) words.push(currentWord);
+
+    return words;
 }
