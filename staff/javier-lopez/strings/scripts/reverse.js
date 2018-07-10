0947963468becbdc0fc769e9004b959b0e12d0ae
Javi <javierlopezblasco@gmail.com>
2018-07-10 16:34:32 +0200

    fixing some errors

--- staff/javier-lopez/strings/scripts/reverse.js
+++ staff/javier-lopez/strings/scripts/reverse.js
@@ -1,6 +1,5 @@
 function reverse(string) {
     // TODO reverse string using a standar loop (use of split, reverse, and join is forbidden)
-    var arrayReversed = string.split("").reverse();
-    var wordReversed = arrayReversed.join("");
+    var wordReversed = string.split("").reverse().join("");
     return wordReversed;
 }
