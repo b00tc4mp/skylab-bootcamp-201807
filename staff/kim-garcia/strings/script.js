b7ecc123944440c447d7eac4998ccbf157f143af
Kimbali <kimgarcianton@hotmail.com>
2018-07-10 13:13:57 +0200

    mec

--- staff/kim-garcia/strings/script.js
+++ staff/kim-garcia/strings/script.js
@@ -1,21 +1,23 @@
-//count words
- function countWords(string){
-     //TODO: count words in string using a standar loop (split and trim are forbiden). remember words not characters
 
- }
+
+// CORRECCION CON REGRETS
 
  console.log(countWords("hello world")=== 2) // ==> true
  console.log(countWords(" ")=== 0) // ==> true
  console.log(countWords(" 1 2 3 4 5")=== 5) // ==> true
  console.log(countWords("    ")=== 0) // ==> true
- console.log(countWords("one two     thre    for five")=== 5) // ==> true
+ console.log(countWords("one two     three    for five")=== 5) // ==> true
+ console.log(countWords("hello world  /t /n") ===2)
+ console.log(countWords("::: ;;; ...") === 0)
+
 
  //split
  function splitToWords(string){
      //todo implement standars loop
+
  }
 var words = splitToWords("hello world")
-console.log(words.lenght ===2) //true
+//console.log(words.length ===2) //true
 console.log(words[0] === "hello") //true
 console.log(words[1] === "world")
  
@@ -49,7 +51,7 @@ console.log(words.length === 3)
 
 // count words
 
-function countWords(string) {
+function countWord(string) {
     // TODO: count words in string using a standard loop (split and trim are forbidden), taking into account spaces and tabs
 }
 
