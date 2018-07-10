e728e65abee49c1cca065e451edeef86b83b1d58
Maider <info@maiderhernandorena.com>
2018-07-10 13:12:48 +0200

    string changes

--- staff/maider-hernandorena/strings/main.js
+++ staff/maider-hernandorena/strings/main.js
@@ -1,4 +1,5 @@
 //count words
+/*
 function countWords(string){
     //TO DO: count words in string using a standard loop (split and trim are forbidden), taking into account spaces and tabs
     function space(value) {
@@ -27,17 +28,36 @@ console.log(countWords(" ") === 0); // => true
 console.log(countWords("    ") === 0); // => true
 console.log(countWords("1 2 3 4 5") === 5); // => true
 console.log(countWords("one two     three    four       five") === 5); // => true
-
+*/
 
 // split to words
 function splitToWords(string) {
-    // TODO implement using standard loop   
-    var result = string.split(" ");
-    var last = result.length - 1;
-    for(var i = 0; i < string[i]; i++){
-        last[i]+=" ";
-    }    
-    return result;
+    var space = " ",
+        word = "",
+        wordsArr = [],
+        symbols = [",", ".", ";", ":"];
+
+        // return /[^\w^\sà-úÀ-Úä-üÄ-Üâ-ûñç]/.test(string);
+
+    for (var j = 0; j < string.length; j++) {
+        if (symbols.indexOf(string[j]) !== -1) {
+            string[j] = "";
+        }
+    }
+
+    for (var i = 0; i < string.length; i++) {
+        var letter = string[i],
+            nextLetter = string[i+1];
+        if (letter !== space) {
+            word += letter;
+            if (!nextLetter || nextLetter === space) {
+               // debugger;
+                wordsArr.push(word);
+                word = "";
+            }
+        } 
+    }
+    return wordsArr;
 }
 
 var words = splitToWords('hello world');
@@ -53,7 +73,12 @@ console.log(words[2] === 'c'); // => true
 
 var words = splitToWords('      ');
 console.log(words.length === 0); // => true
-
+var words = splitToWords(' hello world');
+console.log(words[0] === 'hello'); // => true
+var words = splitToWords('hello world ');
+console.log(words[0] === 'hello'); // => true
+var words = splitToWords('. hello world');
+console.log(words[0] === 'hello'); // => true
 
 /*
 // find words (that match expression in provided function)
