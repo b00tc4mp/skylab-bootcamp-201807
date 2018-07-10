2510f003a43f8e540cdf4589abc930e935d0dece
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-07-10 10:19:35 +0200

    add new validation

--- staff/giorgio-delabarrera/strings/main.js
+++ staff/giorgio-delabarrera/strings/main.js
@@ -6,6 +6,8 @@ console.log(countWords('') === 0);
 console.log(countWords('1 2 3 4 5') === 5);
 console.log(countWords(' 	') === 0);
 console.log(countWords('one   two       three   four     five') === 5);
+console.log(countWords('hola mundo \t\n') === 2);
+console.log(countWords('... ,,, ;;; :::') === 0);
 
 
 console.log('\n--- splitToWords ---');
