507420699067e3677e494e802f489aa9f480e1c1
robert <pulpozero@gmail.com>
2018-07-10 17:06:57 +0200

    array stuff

---
+++ staff/robert-anthony/arrays/scripts/reduce.test.js
@@ -0,0 +1,17 @@
+/*
+function reduce(array, func, initial) {
+  var accumulator = initial || 0;
+  for (var i = 0; i < array.length; i++) {
+    func(accumulator,array[i]);
+  }
+  return accumulator;
+}
+*/
+
+var numbersArr = [1,2,3,4,5,6,7];
+var stringsArr = ["hi","there","what"];
+
+console.log(reduce(numbersArr,function(){return} ))
+
+
+
