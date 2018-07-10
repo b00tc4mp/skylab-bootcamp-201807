2ceb86c1492dece6a367ca097984f9f3efee921b
JuanSensio <juansensio03@gmail.com>
2018-07-10 17:42:37 +0200

    arrays update

--- staff/juan-sensio/arrays/scripts/for-each.js
+++ staff/juan-sensio/arrays/scripts/for-each.js
@@ -1,3 +1,8 @@
 function forEach(array, func) {
     // TODO implement a for-each (use of Array.prototype.forEach is forbidden)
+    var newArray = new Array(array.length);
+    for(var i=0; i<array.length; i++) {
+        newArray[i] = func(array[i]);
+    }
+    return newArray;
 }
