b4db315a03cb6306651a6d6c22e75f024b408093
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-07-10 23:59:47 +0200

    add function for each

--- staff/giorgio-delabarrera/arrays/scripts/highest-number.js
+++ staff/giorgio-delabarrera/arrays/scripts/highest-number.js
@@ -1,15 +1,16 @@
+
 function highestNumber(numbers) {
     // TODO return the highest number found in numbers (use of Math max or min is forbidden)
-
-    if (numbers) {
-        var beforeNumber = 0;
+    
+    var highestNumber = undefined;
+    if (numbers.length) {
+        highestNumber = 0;
         for (var i = 0; i < numbers.length; i++) {
             var number = numbers[i];
-            
-            
-
-            
+            if (isNumber(number)) {
+                highestNumber = (number > highestNumber) ? number : highestNumber;
+            }
         }
     }
-
+    return highestNumber;
 }
