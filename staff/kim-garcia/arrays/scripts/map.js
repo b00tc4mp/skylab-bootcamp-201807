0c6e1f4f3bf7b95c0dede2b4c6080d968444f69d
Kimbali <kimgarcianton@hotmail.com>
2018-07-11 12:38:27 +0200

    poco mas

--- staff/kim-garcia/arrays/scripts/map.js
+++ staff/kim-garcia/arrays/scripts/map.js
@@ -1,4 +1,29 @@
 function map(array, func) {
     // TODO implement a map (it should work same to Array.prototype.map, but accepting the array as a parameter. use of Array.prototoype.map is forbidden)
-    
+    var arr = []
+    for (var i = 0; )
+
 }
+
+
+///FOR EACH
+function forEach(array, func) {
+    // TODO implement a for-each (use of Array.prototype.forEach is forbidden)
+    var strings = []
+    for (i = 0; i < array.length; i++ ){
+       var num = func(array[i]);
+       strings.push(num);
+    }
+    return strings
+}
+
+// test 1
+
+var numbers = [1, 2, 3];
+
+var strings = forEach(numbers, function(number) { return number.toString(); });
+
+console.log(strings.length === 3);
+console.log(strings[0] === '1');
+console.log(strings[1] === '2');
+console.log(strings[2] === '3');
