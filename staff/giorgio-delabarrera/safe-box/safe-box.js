0c386eb5e60580d3bc4ae5943e8fd6236417af9d
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-07-11 20:27:30 +0200

    add safe box with iffe function

---
+++ staff/giorgio-delabarrera/safe-box/safe-box.js
@@ -0,0 +1,51 @@
+
+var safeBox = (function() {
+
+    var keys = {
+        password: '',
+        secret: ''
+    };
+
+    return {
+        /**
+         * Keeps the secret safe
+         * 
+         * @param {string} password The access password
+         * @param {string} secret The secret to keep safe
+         * 
+         * @throws {Error} If password is wrong
+         */
+        keep: function(password, secret) {
+            // TODO
+            console.log(keys);
+            
+        },
+
+        /**
+         * Retrieves the secret
+         * 
+         * @param {string} password The access password
+         * 
+         * @returns {string} The secret
+         * 
+         * @throws {Error} If wrong password
+         */
+        retrieve: function(password) {
+            // TODO
+            
+        },
+
+        /**
+         * Updates the access password
+         * 
+         * @param {string} password The current password
+         * @param {string} newPassword The new password
+         * 
+         * @throws {Error} If current and/or new password are wrong
+         */
+        updatePassword(password, newPassword) {
+            // TODO
+        },
+    };
+
+})();
