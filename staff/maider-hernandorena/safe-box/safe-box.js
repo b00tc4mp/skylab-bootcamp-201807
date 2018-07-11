c47ec3fbbe225cbf864e988361433d6e73f97cca
Maider <info@maiderhernandorena.com>
2018-07-11 16:13:43 +0200

    mergeado safebox

--- staff/maider-hernandorena/safe-box/safe-box.js
+++ staff/maider-hernandorena/safe-box/safe-box.js
@@ -1,5 +1,38 @@
-var safeBox = (
-    //TO DO
-    
+var safeBox = {
+    /**
+     * Keeps the secret safe
+     * 
+     * @param {string} password The access password
+     * @param {string} secret The secret to keep safe
+     * 
+     * @throws {Error} If password is wrong
+     */
+    keep: function(password, secret) {
+        // TODO
+    },
 
-)
+    /**
+     * Retrieves the secret
+     * 
+     * @param {string} password The access password
+     * 
+     * @returns {string} The secret
+     * 
+     * @throws {Error} If wrong password
+     */
+    retrieve: function(password) {
+        // TODO
+    },
+
+    /**
+     * Updates the access password
+     * 
+     * @param {string} password The current password
+     * @param {string} newPassword The new password
+     * 
+     * @throws {Error} If current and/or new password are wrong
+     */
+    updatePassword(password, newPassword) {
+        // TODO
+    }
+};
