6ee069d552582a06a47506896e7c1ea3d17ae438
laia89 <laiacy234@gmail.com>
2018-07-11 20:57:06 +0200

    solve array highest-number problem

--- staff/laia-chorro/arrays/scripts/highest-number.js
+++ staff/laia-chorro/arrays/scripts/highest-number.js
@@ -1,3 +1,14 @@
 function highestNumber(numbers) {
     // TODO return the highest number found in numbers (use of Math max or min is forbidden)
+    var maxNum;
+
+    for (var i = 0; i < numbers.length; i++) {
+        var num = Number(numbers[i]);
+
+        if (!Number.isNaN(num) && (!maxNum || num > maxNum)) {
+            maxNum = num;
+        }
+    } 
+
+    return maxNum;
 }
