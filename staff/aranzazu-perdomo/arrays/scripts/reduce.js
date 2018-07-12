7ce78c7eaf2c8110f13b4ebe20e10e18c7bdcabb
Aranzazu Perdomo <huella85@example.com>
2018-07-12 12:59:49 +0200

    add-safe-box

---
+++ staff/aranzazu-perdomo/arrays/scripts/reduce.js
@@ -0,0 +1,10 @@
+function reduce(array, func, init) {
+    var arr= [];
+    for (var i=0; i<array.length; i++){
+        if(i==array.length-1){
+            arr.push(func(array[i]));
+        }
+
+    }
+    return arr;
+}
