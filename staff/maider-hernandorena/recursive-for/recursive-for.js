d328f27aa78952d6d6c1a470f5944e6eadcef97c
Maider <info@maiderhernandorena.com>
2018-07-12 12:59:02 +0200

    recursive for more cases

--- staff/maider-hernandorena/recursive-for/recursive-for.js
+++ staff/maider-hernandorena/recursive-for/recursive-for.js
@@ -1,17 +1,27 @@
-var count = 0;
+// opción 1
+function recursiveFor(numbers, fun, index) {
+    // DONE implement a for-loop without for, but just recursion
+    index = index || 0;
+    if (index < numbers.length) {
+        fun(numbers[index]);
+        recursiveFor(numbers, fun, ++index);
+    } 
+};
 
-function recursiveFor(numbers, fun) {
-    // TODO implement a for-loop without for, but just recursion
-    if (mul2.length != 3) {
-        fun(numbers[count]);
-        count++
+// opción 2 (mal porque cualquiera podría modificar nuestro index)
+var index = 0;
+function _recursiveFor(numbers, fun) {
+    // DONE implement a for-loop without for, but just recursion
+    if (index < numbers.length) {
+        fun(numbers[index]);
+        index++;
         recursiveFor(numbers, fun);
     } 
 };
 
-// opcion Manu
+// opción 3 Manu
 function _recursiveFor(array, callback) {
-    // TODO implement a for-loop without for, but just recursion
+    // DONE implement a for-loop without for, but just recursion
     if (array.length) {
         callback(array[0]);
         recursiveFor(array.slice(1), callback);
