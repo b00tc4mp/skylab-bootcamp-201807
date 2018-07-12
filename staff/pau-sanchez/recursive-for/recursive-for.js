3986938d1c21fccc351bdb020c08d696e0332d4a
Pau <idea.barcelona@gmail.com>
2018-07-12 12:58:59 +0200

    reverse

---
+++ staff/pau-sanchez/recursive-for/recursive-for.js
@@ -0,0 +1,32 @@
+
+
+function recursiveFor(numbers, func) {
+        
+    
+    if (numbers.length){
+        func(numbers[0])
+        recursiveFor(numbers.slice(1),func);
+    }
+    
+}
+
+//////TEST////////////
+
+
+var numbers = [1, 2, 3];
+var mul2 = [];
+
+recursiveFor(numbers, function(value) {
+    mul2.push(value * 2);
+});
+
+assert('mul2 length is 3', mul2.length === 3);
+assert('mul2 item 0 is 2', mul2[0] === 2);
+assert('mul2 item 1 is 4', mul2[1] === 4);
+assert('mul2 item 2 is 6', mul2[2] === 6);
+
+
+
+
+
+
