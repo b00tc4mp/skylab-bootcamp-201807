29dcda83e467de7a5941e9e51a613ace9a894897
Javi <javierlopezblasco@gmail.com>
2018-07-12 09:07:40 +0200

    safe-box finished and add all elements exercise fixed

--- staff/javier-lopez/arrays/scripts/add-all-numbers.js
+++ staff/javier-lopez/arrays/scripts/add-all-numbers.js
@@ -1,7 +1,7 @@
 // add all numbers
 //Use only for or while
 function addAllElements(array){
-    res = 0;
+    var res = 0;
         for(var i = 0;i<array.length;i++){
             res = res + array[i];
         }
