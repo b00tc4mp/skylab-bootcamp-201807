4c46f3fdfedf9b9af00de1f53c033526cb9b2cb3
laia89 <laiacy234@gmail.com>
2018-07-14 13:18:55 +0200

    create a test for Array.from method

---
+++ staff/laia-chorro/arrays-native/scripts/from.test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+describe('Array.from', function() {
+
+    it ('should iterate and create an array copy from the chars contained in the string', function() {
+        var string = 'foo';
+        var arrayOfChars = Array.from(string);
+
+        expect(arrayOfChars.length).toBe(3);
+        expect(arrayOfChars[0]).toBe('f');
+        expect(arrayOfChars[1]).toBe('o');
+        expect(arrayOfChars[2]).toBe('o');
+    });
+
+    it ('should return an empty array from a number', function() {
+        var singleNum = 1,
+            emptyArray = Array.from(singleNum);
+
+        expect(emptyArray.length).toBe(0);
+        expect(emptyArray.constructor === Array).toBe(true);
+    });
+});
+
+
