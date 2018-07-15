1b432b3b89588be6e21068e692f2f2ff530ed3d5
Aranzazu Perdomo <huella85@example.com>
2018-07-15 16:40:05 +0200

    add new change

--- staff/aranzazu-perdomo/arrays/scripts/highest-number.js
+++ staff/aranzazu-perdomo/arrays/scripts/highest-number.js
@@ -1,15 +1,14 @@
 function highestNumber(numbers) {
-  var max=numbers[0];
-  for(var i=1; i<numbers.length ; i++){
-        if(numbers[i] > max){
-            max=numbers[i]
+  
+  var max = numbers[0];
+
+    for (var i = 0; i < numbers.length; i++) {
+        if (max < numbers[i]) {
+            max = numbers[i];
         }
-
-
-  }
-return max;
-
+    }
+    return max;
+} 
 
 
 
-}
