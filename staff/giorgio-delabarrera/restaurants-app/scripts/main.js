db4c037ece411e778a43311e27a586ccda631aaa
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-07-15 16:30:13 +0200

    add restaurant app stable

--- staff/giorgio-delabarrera/restaurants-app/scripts/main.js
+++ staff/giorgio-delabarrera/restaurants-app/scripts/main.js
@@ -1,113 +1,78 @@
 // my custom components
 
-function SearchPanel(tag) {
+function SearchPanel() {
     
-    Component.call(this, tag);
+    Component.call(this);
 
     // input
     var input = document.createElement('input');
-    input.setAttribute('type', 'text');
-    input.setAttribute('id', 'search');
-    input.setAttribute('value', 'rris');
+    input.type = 'search';
+    input.placeholder = 'Input a text...';
 
     // button
     var button = document.createElement('button');
-    button.setAttribute('type', 'submit');
-    button.innerText = 'Buscar';
+    button.type = 'submit';
+    button.innerText = 'Search';
 
     // form
-    var form = document.createElement('form');
-    form.appendChild(input);
-    form.appendChild(button);
+    this.element = document.createElement('form');
+    this.element.appendChild(input);
+    this.element.appendChild(button);
 
-    this.element.appendChild(form);
-
-    // this._onSearch = function(event) { event.preventDefault(); alert('ahora?') };
     this._onSearch = function() {};
 
-    form.addEventListener('submit', function(event) {
+    this.element.addEventListener('submit', function(event) {
         event.preventDefault();
-        this._onSearch(input.value);
-    });
+        var query = input.value;
+        
+        if (query) this._onSearch(query);
 
-    form.addEventListener('submit', this._onSearch);
+    }.bind(this));
 }
 
 SearchPanel.prototype = Object.create(Component.prototype);
 SearchPanel.prototype.constructor = SearchPanel;
+
 SearchPanel.prototype.onSearch = function(callback) {
     this._onSearch = callback;
 };
 
-// function handleSearch(input) {
-function handleSearch(event) {
 
-    event.preventDefault();
-    alert('submitttt');
-
-    // var re = new RegExp(input); 
-
-    // var searchedRestaurants = [{"address": {"building": "469", "coord": [-73.961704, 40.662942], "street": "Flatbush Avenue", "zipcode": "11225"}, "borough": "Brooklyn", "cuisine": "Hamburgers", "grades": [{"date": {"$date": 1419897600000}, "grade": "A", "score": 8}, {"date": {"$date": 1404172800000}, "grade": "B", "score": 23}, {"date": {"$date": 1367280000000}, "grade": "A", "score": 12}, {"date": {"$date": 1336435200000}, "grade": "A", "score": 12}], "name": "Wendy'S", "restaurant_id": "30112340"}];
-
-    // console.log(searchedRestaurants);
-
-    // var searchedRestaurants = restaurants.filter(function(restaurant) {
-    //     var searched = false;
-    //     for (var property in restaurant) {
-            
-    //         var value = restaurant[property];
-    //         if (typeof restaurant[property] === 'string') {
-    //             value = restaurant[property].toLowerCase();
-    //         }
-
-    //         if (re.test(value) === true) {
-    //             searched = true;
-    //             break;
-    //         } 
-    //     }
-    //     return searched;
-    // });
-
-    // console.log(searchedRestaurants);
-}
-
-function ResultsList(tag, restaurants) {
-    Component.call(this, tag);
-
-    var ul = document.createElement('ul');
-
-    restaurants.forEach(function(restaurant){
-
-        var li = document.createElement('li');
-        li.innerText = restaurant.name + ', ' +restaurant.borough + ', ' + restaurant.cuisine;
-        
-        ul.appendChild(li);
-    });
-    
-    this.element.appendChild(ul);
+function ResultsList() {
+    Component.call(this, 'ul');
 }
 
 ResultsList.prototype = Object.create(Component.prototype);
 ResultsList.prototype.constructor = ResultsList;
 
+ResultsList.prototype.updateResults = function(results) {
+    this.element.innerHTML = '';
 
-// function search(word, restaurants) {
-//     return [{"address": {"building": "1007", "coord": [-73.856077, 40.848447], "street": "Morris Park Ave", "zipcode": "10462"}, "borough": "Bronx", "cuisine": "Bakery", "grades": [{"date": {"$date": 1393804800000}, "grade": "A", "score": 2}, {"date": {"$date": 1378857600000}, "grade": "A", "score": 6}, {"date": {"$date": 1358985600000}, "grade": "A", "score": 10}, {"date": {"$date": 1322006400000}, "grade": "A", "score": 9}, {"date": {"$date": 1299715200000}, "grade": "B", "score": 14}], "name": "Morris Park Bake Shop", "restaurant_id": "30075445"}];
-// }
+    results.forEach(function(result) {
+        var li = document.createElement('li');
+
+        li.innerHTML = result;
+
+        this.element.appendChild(li);
+    }, this);
+}
 
 
 // my logic ...
 
-// console.log(restaurants);
-var filteredRestaurants = restaurants;
+var searchPanel = new SearchPanel();
 
-var searchPanel = new SearchPanel('section');
-searchPanel.onSearch(handleSearch);
-// searchPanel.onSearch(function(event) { event.preventDefault(); });
-// searchPanel.test = function() { alert('pepe') };
+var resultsList = new ResultsList();
+
+searchPanel.onSearch(function(query) {
+    var matching = restaurants.filter(function(restaurant) {
+        return restaurant.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    });
+
+    console.log(matching);
+    
+    resultsList.updateResults(matching.map(function(restaurant) { return restaurant.name; }));
+});
+
 document.body.appendChild(searchPanel.element);
-
-var resultList = new ResultsList('section', filteredRestaurants);
-document.body.appendChild(resultList.element);
-
-console.log(searchPanel);
+document.body.appendChild(resultsList.element);
