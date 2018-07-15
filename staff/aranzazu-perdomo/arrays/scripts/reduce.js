8382adc395042bc7005fbf1c3c4ce5397600d182
Aranzazu Perdomo <huella85@example.com>
2018-07-15 16:16:25 +0200

    add new change

--- staff/aranzazu-perdomo/arrays/scripts/reduce.js
+++ staff/aranzazu-perdomo/arrays/scripts/reduce.js
@@ -1,11 +1,19 @@
 function reduce(array, func, init) {
-    var arr= [];
-    for (var i=0; i<array.length; i++){
-        if(i==array.length-1){
-            arr.push(func(array[i]));
-
-        }
-
-    }
-    return arr;
-}
+       
+       var accum = init;
+       
+       for (var i = 0; i < array.length; i++){
+           
+           accum =+ func(accum, array[i]);
+           
+           var sum = accum;
+           var numberOfWordsWithL = accum;
+           var total = accum;
+           
+       }
+       
+       return sum;
+       return numberOfWordsWithL;
+       return total;
+   
+   }
--- staff/aranzazu-perdomo/arrays/scripts/sort.js
+++ staff/aranzazu-perdomo/arrays/scripts/sort.js
@@ -1,25 +1,33 @@
 
 function sort(array, reverse) {
-   
-    for (var i=0; i<array.length; i++){
-        for(var j=0; j<array.length; j++){
-           
-           if(!reverse){
-            if(array[i]<array[j]){
-                var temp=array[i];
-                array[i]=array[j];
-                array[j]=temp;
-            };
-            };else if{
-                array[i]=temp;
-                array[j]=array[i];
-                temp=array[j];
-               
-            };
-
+        // TODO return array sorted alphabetically 
+        // numerically (use of Array.prototype.sort is forbidden)
+        
+        var arrayA = [];
+    
+        var arrayB = [array[0]];
+    
+        for (var i = array.length; i > 0; i--){
+            for (var j = 0; j <= array.length; j++){
+              if (reverse === undefined){  
+                if (array[j] < arrayB[0]){
+                    arrayB.push(array[j]);
+                    arrayB.shift();
+                }
+             }else if(reverse === true){
+                if (array[j] > arrayB[0]){
+                    arrayB.push(array[j]);
+                    arrayB.shift();
+                }
+             }
+            }
+            array.splice(array.indexOf(arrayB[0]),1);
+            arrayA.push(arrayB[0]);
+            arrayB = [array[0]];
+            
         }
-
-
-    }
-    return array;
-}
+        
+        
+            
+            return arrayA;
+        }
