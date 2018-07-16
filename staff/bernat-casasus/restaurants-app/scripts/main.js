680564b0e929aba224dc49f6bf028c54d0d197e5
Bernat <bernatcasasus97@gmail.com>
2018-07-16 11:43:35 +0200

    copy manu restaurant app

--- staff/bernat-casasus/restaurants-app/scripts/main.js
+++ staff/bernat-casasus/restaurants-app/scripts/main.js
@@ -1,71 +1,82 @@
 // my custom components
 
-function SearchPanel(tag) {
-    // TODO
-    Component.call(this,tag);
-    var form = document.createElement("form");
-    var input= document.createElement("input");
-    var button = document.createElement("button");
-    input.type = "text";
-    input.id = "searchInput"
+function SearchPanel() {
+    Component.call(this, 'form');
+
+    var input = document.createElement('input');
+    input.type = 'search';
+    input.placeholder = 'Input a text...';
+
+    var button = document.createElement('button');
+    button.type = 'submit';
     button.innerHTML = 'Search';
-    button.type = "submit";
-    button.id = "searchButton";
-    form.appendChild(input);
-    form.appendChild(button);
-    this.element.appendChild(form);
+
+    this.element.appendChild(input);
+    this.element.appendChild(button);
+
+    this._callback = function () { };
+
+    // this.element.onsubmit = function(event) {
+    //     event.preventDefault();
+
+    //     var query = input.value;
+
+    //     this._callback(query);
+    // }.bind(this);
+
+    this.element.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        var query = input.value;
+
+        if (query) this._callback(query);
+    }.bind(this));
 }
 
 SearchPanel.prototype = Object.create(Component.prototype);
 SearchPanel.prototype.constructor = SearchPanel;
 
-function ResultsList(tag,array) {
+SearchPanel.prototype.onSearch = function (callback) {
+    this._callback = callback;
+};
 
-    // TODO
-    Component.call(this,tag);
-    var ul = document.createElement("ul");
-
-    this.element.id = "resultsContainer";
-    array.forEach(function(value){
-
-        var li = document.createElement("li");
-        li.innerText = value.name;
-
-        ul.appendChild(li);
-    })
-
-    this.element.appendChild(ul);
+function ResultsList() {
+    Component.call(this, 'ul');
 }
 
 ResultsList.prototype = Object.create(Component.prototype);
 ResultsList.prototype.constructor = ResultsList;
 
+ResultsList.prototype.updateResults = function(results) {
+    this.element.innerHTML = '';
+
+    results.forEach(function(result) {
+        var li = document.createElement('li');
+
+        li.innerHTML = result;
+
+        this.element.appendChild(li);
+    }, this);
+};
+
 // my logic ...
 
-    var filter = restaurants.slice(0, 1000);
+// optional, reduce the size of the restaurants loaded in memory
+// restaurants.splice(1000);
 
-    var searchPanel = new SearchPanel('section');
-    document.body.appendChild(searchPanel.element);
+var search = new SearchPanel();
 
-    var resultsList = new ResultsList('section',filter);
-    document.body.appendChild(resultsList.element);
-
-
-    document.getElementById("searchButton").addEventListener("click", function(event){
-        event.preventDefault();
-        
-        var inputValue = document.getElementById('searchInput').value;
-
-        var searchResults = [];
-        filter.forEach(function(value){
-            if (Object.values(value).indexOf(inputValue) > -1) {
-                console.log('has '+inputValue);
-                searchResults.push(value);
-             }
-        })
-
-
-        var resultsList = new ResultsList('section',searchResults);
-        document.getElementById("resultsContainer").innerHTML = resultsList.element.innerHTML;
-
+search.onSearch(function (query) {
+    var matching = restaurants.filter(function(restaurant) {
+        return restaurant.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
     });
+
+    //console.log(matching);
+
+    results.updateResults(matching.map(function(result) { return result.name; }));
+});
+
+var results = new ResultsList();
+
+document.body.appendChild(search.element);
+document.body.appendChild(results.element);
