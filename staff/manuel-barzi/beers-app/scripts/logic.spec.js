9eeb87946e7f65976121a860355d424797e2d3b3
manuelbarzi <manuelbarzi@gmail.com>
2018-07-17 16:46:58 +0200

    update solution to beers-app

--- staff/manuel-barzi/beers-app/scripts/logic.spec.js
+++ staff/manuel-barzi/beers-app/scripts/logic.spec.js
@@ -4,55 +4,29 @@ describe('logic (beers)', function () {
     describe('search beers', function () {
         var foundBeers;
 
-        // beforeEach(function (done) {
-        //     logic.searchBeers('mahou', function (beers) {
-        //         foundBeers = beers;
+        it('should find 4 beers matching criteria', function () {
+            return logic.searchBeers('mahou')
+                .then(function (beers) {
+                    foundBeers = beers;
 
-        //         done();
-        //     });
-        // });
+                    expect(foundBeers).toBeDefined();
+                    expect(foundBeers.length).toBe(4);
 
-        // it('should find 4 beers matching criteria "mahou"', function () {
-        //     expect(foundBeers).toBeDefined();
-        //     expect(foundBeers.length).toBe(4);
-        // });
+                    var beer = foundBeers[0];
 
-        // it('should return beer "Mahou Maestra" with id "8OucfG" in first place', function () {
-        //     var beer = foundBeers[0];
-
-        //     expect(beer.name).toBe('Mahou Maestra');
-        //     expect(beer.id).toBe('8OucfG');
-        // });
-
-        it('should find 4 beers matching criteria', function (done) {
-            logic.searchBeers('mahou', function (error, beers) {
-                expect(error).toBeUndefined();
-
-                foundBeers = beers;
-
-                expect(foundBeers).toBeDefined();
-                expect(foundBeers.length).toBe(4);
-
-                var beer = foundBeers[0];
-
-                expect(beer.name).toBe('Mahou Maestra');
-                expect(beer.id).toBe('8OucfG');
-
-                done();
-            });
+                    expect(beer.name).toBe('Mahou Maestra');
+                    expect(beer.id).toBe('8OucfG');
+                });
         });
     });
 
     describe('retrieve beer by id', function () {
-        it('should retrieve beer matching the id', function (done) {
-            logic.retrieveBeerById('8OucfG', function (error, beer) {
-                expect(error).toBeUndefined();
-                
-                expect(beer.name).toBe('Mahou Maestra');
-                expect(beer.id).toBe('8OucfG');
-
-                done();
-            });
+        it('should retrieve beer matching the id', function () {
+            return logic.retrieveBeerById('8OucfG')
+                .then(function (beer) {
+                    expect(beer.name).toBe('Mahou Maestra');
+                    expect(beer.id).toBe('8OucfG');
+                });
         });
     });
 });
