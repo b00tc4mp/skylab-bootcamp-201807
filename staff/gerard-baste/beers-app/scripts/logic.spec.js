41b82b248412684209d0981c707d0ac234c8c6cd
gbaste <gbfabre@gmail.com>
2018-07-17 11:50:37 +0200

    add beers

---
+++ staff/gerard-baste/beers-app/scripts/logic.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('logic (beers)', function () {
+    var beersArray;
+
+    beforeEach(function(done) {
+        logic.searchBeers('mahou', function(beers) {
+             beersArray = beers;
+             done();
+            });
+    });
+
+    it('should results length be 4', function() {
+        expect(beersArray.length).toBe(4);
+    });
+    
+});
