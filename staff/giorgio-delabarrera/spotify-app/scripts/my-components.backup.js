cf2b46dab22e8e347c349f51ad0e956d96adba63
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-07-18 18:37:01 +0200

    add spotify-app with jQuery

--- staff/giorgio-delabarrera/spotify-app/scripts/my-components.backup.js
+++ staff/giorgio-delabarrera/spotify-app/scripts/my-components.backup.js
@@ -44,14 +44,16 @@ ResultsList.prototype.constructor = ResultsList;
 ResultsList.prototype.updateResults = function (results) { // => { id, text }
     
     var $element = $(this.element);
-
+    
+    // $element.html('');
     $element.empty();
+    // this.element.innerHTML = '';
 
     $.each(results, function(index, result) {
 
         var $li = $('<li></li>');
 
-        var $a = $('<a></a>')
+        var $a = $('<a>')
             .attr('href', '#/' + result.id)
             .html(result.text);
 
@@ -62,8 +64,22 @@ ResultsList.prototype.updateResults = function (results) { // => { id, text }
         $li.append($a);
 
         $element.append($li);
+    });
 
-    }.bind(this));
+    // results.forEach(function (result) {
+    //     var li = document.createElement('li');
+    //     var a = document.createElement('a');
+
+    //     a.href = '#/' + result.id;
+    //     a.innerHTML = result.text;
+    //     a.onclick = function () {
+    //         if (this._callback) this._callback(result.id, result.text);
+    //     }.bind(this);
+
+    //     this.element.appendChild(li);
+
+    //     li.appendChild(a);
+    // }, this);
 };
 
 ResultsList.prototype.onItemClick = function (callback) {
@@ -75,34 +91,35 @@ ResultsList.prototype.onItemClick = function (callback) {
  * @param {string} title The item title
  * @param {string} info The information about an item
  * @param {string} image The image of the item
- * @param {string} preview The image url to the item cover 
  */
 function DetailPanel(title, info, image, preview) {
     Panel.call(this, title, 'section');
 
-    var $element = $(this.element);
+    var p = document.createElement('p');
+    p.innerText = info;
 
-    var $p = $('<p></p>')
-        .text(info);
+    this.element.appendChild(p);
 
-    $element.append($p);
+    var img = document.createElement('img');
+    img.src = image;
 
-    var $img = $('<img>')
-        .attr('src', image);
+    // img.width = '512';
+    // img.height = '512';
+    // img.classList.add('test');
 
-    $element.append($img);
+    this.element.appendChild(img);
 
     if (preview) {
-        var $audio = $('<audio></audio>')
-            .prop('controls', true);
+        var audio = document.createElement('audio');
+        audio.controls = true
         
-        var $source = $('<source></source>')
-            .attr('src', preview)
-            .attr('type', 'audio/mpeg');
+        var source = document.createElement('source');
+        source.src = preview;
+        source.type = 'audio/mpeg';
 
-        $audio.append($source);
+        audio.appendChild(source);
 
-        $element.append($audio);
+        this.element.appendChild(audio);
     }
 }
 
@@ -110,5 +127,5 @@ DetailPanel.prototype = Object.create(Panel.prototype);
 DetailPanel.prototype.constructor = DetailPanel;
 
 DetailPanel.prototype.getAudio = function() {
-    return $(this.element).find('audio')[0];
+    return this.element.querySelector('audio');
 }
