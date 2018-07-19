8154458b7e57251405d3dc0758392a6f46193e9c
Jordi Ubanell <jordi@jordiubanell.com>
2018-07-19 17:26:56 +0200

    spotify version bootstrapp 00

--- staff/jordi-ubanell/spotify-app/scripts/logic.js
+++ staff/jordi-ubanell/spotify-app/scripts/logic.js
@@ -2,56 +2,40 @@ var logic = {
     token: 'NO-TOKEN',
 
     _callApi: function (path) {
-        return new Promise(function (resolve, reject) {
-            var request = new XMLHttpRequest();
-
-            request.onreadystatechange = function () {
-                if (request.readyState === 4) {
-                    if (request.status === 200) {
-                        var res = JSON.parse(request.responseText);
-
-                        resolve(res);
-                    } else reject(Error('request error, status ' + request.status));
-                }
-            };
-
-            var url = 'https://api.spotify.com/v1' + path;
-
-            request.open('get', url);
-
-            request.setRequestHeader('Authorization', 'Bearer ' + this.token);
-
-            request.send();
-        }.bind(this));
+        return $.ajax('https://api.spotify.com/v1-' + path, {
+            headers: {
+                authorization: 'Bearer ' + this.token
+            }
+        })
+        .catch(function(err) {
+            throw Error('request error, status ' + err.status);
+        });
     },
 
     searchArtists: function (query) {
         return this._callApi('/search?type=artist&query=' + query)
-            .then(function (res) {
+            .then(function(res) {
                 return res.artists.items;
             });
     },
 
     retrieveAlbumsByArtistId(id) {
         return this._callApi('/artists/' + id + '/albums')
-            .then(function (res) {
+            .then(function(res) {
                 return res.items;
             });
     },
 
     retrieveTracksByAlbumId(id) {
-        // TODO
         return this._callApi('/albums/' + id + '/tracks')
-            .then(function (res) {
+            .then(function(res) {
                 return res.items;
             });
     },
 
-
     retrieveTrackById(id) {
-        // TODO
         return this._callApi('/tracks/' + id)
-            .then(function (res) {
+            .then(function(res) {
                 return res;
             });
     }
--- staff/jordi-ubanell/spotify-app/scripts/logic.spec.js
+++ staff/jordi-ubanell/spotify-app/scripts/logic.spec.js
@@ -1,8 +1,8 @@
 'use strict';
 
 describe('logic (spotify)', function () {
-    logic.token = 'BQBR4LH-AR-LMprdvBtMSSJFudIG_0xOAGeqsdLcehpSrzFeCmHv2ryx8iUQ_-9vgLpXpsrEUIz7XcWxz-yri8nbhhTCNkRS4bhZJokQ0DAm-6ccGLfP-X4g9y0aiu_N9tkYAVf2Rr4';
-
+    logic.token = 'BQB5E-cSm2NXdDZZckNZJ20CU-2dfMT-7cXoNMP6BCUNFOfpcPsmAvnVpQt0ZqBILy36KRvEaiQivqONg8JlvbY6QPlWjIBsy17MVZZZUQqnQBq3Zjum__N121LTgO_lgDlDi0VFSWzp';
+    
     describe('search artists', function () {
         it('should find artists matching criteria', function () {
             return logic.searchArtists('madonna')
@@ -10,6 +10,7 @@ describe('logic (spotify)', function () {
                     expect(artists).toBeDefined();
                     expect(artists.length).toBe(20);
                     expect(artists[0].name).toBe('Madonna');
+                    expect(artists[0].type).toBe('artist');
                 });
         });
     });
@@ -21,29 +22,31 @@ describe('logic (spotify)', function () {
                     expect(albums).toBeDefined();
                     expect(albums.length).toBe(3);
                     expect(albums[0].name).toBe('Hunter');
+                    expect(albums[0].type).toBe('album');
                 });
         });
     });
 
     describe('retrieve tracks by album id', function () {
         it('should retrieve tracks for given album id', function () {
-            return logic.retrieveTracksByAlbumId('402yUhwU0GxLYcFESBFddN')
+            return logic.retrieveTracksByAlbumId('7lnYU1xXbEiKPTZk3ltDE2')
                 .then(function (tracks) {
                     expect(tracks).toBeDefined();
                     expect(tracks.length).toBe(1);
-                    expect(tracks[0].name).toBe('Jungle Love');
+                    expect(tracks[0].name).toBe('Hunter');
+                    expect(tracks[0].type).toBe('track');
                 });
         });
     });
 
     describe('retrieve track by id', function () {
-        it('should retrieve a track for given id', function () {
-            return logic.retrieveTrackById('0OqRSUAfZSKitnvyNlJNdE')
+        it('should retrieve track for given id', function () {
+            return logic.retrieveTrackById('4QxwXcPUm1VfkHksz6VuFi')
                 .then(function (track) {
                     expect(track).toBeDefined();
-                    // expect(track.length).toBe(1);
+                    expect(track.name).toBe('Hunter');
+                    expect(track.type).toBe('track');
                 });
         });
-    }); 
+    });
 });
-
