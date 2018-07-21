2f837be040f1d6885697fabbecc50eb872aa24e1
Jordi Ubanell <jordi@jordiubanell.com>
2018-07-21 15:37:11 +0200

    spotify version bootstrapp 06

--- staff/jordi-ubanell/spotify-app/scripts/logic.spec.js
+++ staff/jordi-ubanell/spotify-app/scripts/logic.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('logic (spotify)', function () {
-    logic.token = 'BQBU8wxgmmi28WSmq5jGwepWXwUd2qJdkFDjk7i-YJyyQSpqlgChfflftGEwJGcNSIWh4uuN8DbND0RNOyJRjrUSQrH_usK5QgEPuEXrge0w-Vg8bap_7Zy56_4AdYBvs36EHSEmf34';
+    logic.token = 'BQDtSBsOWX7hPg1bnWizYaFxMxeCzK_JAvCA-snXO3M2e-J5MmQWa7I6QR7xP7Dv1o5mLdNl67phVxp7hPIi9LWVLJTL8kSNomchIZP_p_o29PcYT0zZ5Pf7XPBWuQ1jNSjbReIvLrI';
     
     describe('search artists', function () {
         it('should find artists matching criteria', function () {
