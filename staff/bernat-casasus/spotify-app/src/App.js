97a0bc8293f156fb1447b78df6c0442230aa6083
Bernat <bernatcasasus97@gmail.com>
2018-07-23 21:36:27 +0200

    done spoty-app challenge

--- staff/bernat-casasus/spotify-app/src/App.js
+++ staff/bernat-casasus/spotify-app/src/App.js
@@ -3,18 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import SearchPanel from './components/SearchPanel';
 import ResultList from './components/ResultList';
+import TrackPanel from './components/TrackPanel';
 import logic from './logic'
-logic.token = 'BQDa50ZLyAIiu5HPvW5-vqmmqfO7deePp6lrDW1v57EU2CJ2uCaXj5Np3TINOZyzldw-A6Kw6Yj13SoT0ueeZqekDpuwWwH2L-dd1zKlcq2SrSTt-jRjHpQCkelKWUIcMxjcHn39u3MkT6f_C6RFQDp2W2sb0TK_I70'
+logic.token = 'BQDJVfA66YsdwTzw7zX1yORP3i4VGsuaQfwbvv8SxA3Wa8-xDCIv1lemOjLx-E5ywYPd0VW5LRfifBnv1TosKswvmgrDdmJqu34Acu4rKij_T_QMtXafj5K70QzRfBIri0n7ue4JMlJm-Ft5U6M36FbGACDLuNm7g7Y'
 class App extends Component {
 
-  state = { artists: [], albums: [] }
+  state = { artists: [], albums: [], tracks: [], track: {} }
   onSearch = query => {
     logic.searchArtists(query)
       .then(artists => {
         this.setState({
           artists: artists.map(artist => {
             return { id: artist.id, text: artist.name }
-          })
+          }), albums:[],tracks:[],track:[]
         })
       })
       .catch(console.error)
@@ -27,10 +28,32 @@ class App extends Component {
           this.setState({
             albums: albums.map(album =>{
               return {id: album.id, text: album.name}
-            })
+            }),tracks:[],track:[]
           })
       })
   }
+
+  onAlbumClick = (id) =>{
+    logic.retrieveTracksByAlbumId(id)
+      .then(tracks =>{
+        this.setState({
+          tracks: tracks.map(track => {
+            return {id: track.id, text: track.name}
+          }),track:[]
+        })
+      })
+  }
+
+  onTrackClick = (id) => {
+    logic.retrieveTrackById(id)
+        .then(track => {
+          this.setState({
+            track: {id: track.id, text: track.name, preview_url: track.preview_url}
+          })
+        })
+    
+  }
+
   render() {
     return (
       <div className="App">
@@ -40,6 +63,9 @@ class App extends Component {
         </header>
         <SearchPanel onSearch={this.onSearch} />
         <ResultList results={this.state.artists} onItemClick={this.onArtistClick} />
+        <ResultList results={this.state.albums} onItemClick={this.onAlbumClick} />
+        <ResultList results={this.state.tracks} onItemClick={this.onTrackClick} />
+        <TrackPanel result={this.state.track} />
       </div>
     );
   }
