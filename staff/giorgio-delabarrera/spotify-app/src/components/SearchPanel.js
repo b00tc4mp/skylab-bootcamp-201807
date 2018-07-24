a49da4d48ba8432a7d3316dbdb947bce8559c9e7
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-07-24 11:16:52 +0200

    spotify with style

--- staff/giorgio-delabarrera/spotify-app/src/components/SearchPanel.js
+++ staff/giorgio-delabarrera/spotify-app/src/components/SearchPanel.js
@@ -16,10 +16,17 @@ class SearchPanel extends Component {
 
     render() {
         return (
-            <form onSubmit={this.onSearch}>
-                <input type="text" onChange={this.keepQuery}/>
-                <button type="submit">Search</button>
-            </form>
+            // <form onSubmit={this.onSearch}>
+            //     <input type="text" onChange={this.keepQuery}/>
+            //     <button type="submit">Search</button>
+            // </form>
+
+            <section className="search">
+                <form onSubmit={this.onSearch}>
+                    <label className="search__label">Search artists</label>
+                    <input className="search__input" type="text" placeholder="Input..." onChange={this.keepQuery} />
+                </form>
+            </section>
         );
     }
 }
