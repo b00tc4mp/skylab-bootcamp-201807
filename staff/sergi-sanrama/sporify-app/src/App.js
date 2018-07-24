22c201432993933a25106bd58cef589def7f9368
sergiSanramaT <sergisanramat@gmail.com>
2018-07-24 11:11:14 +0200

    add spotify-react-finally-version

--- staff/sergi-sanrama/sporify-app/src/App.js
+++ staff/sergi-sanrama/sporify-app/src/App.js
@@ -4,40 +4,96 @@ import './App.css';
 import SearchPanel from './components/SearchPanel';
 import logic from './logic'
 import ResultList from './components/ResultList'
+import TrackPlayer from './components/TrackPlayer';
 
-logic.token = 'BQBHEo3SbacdDs_2CPw-DLojiVHn4vReq5ETNyCh789poVkA2jF9jaAO0aMsjwev-2gjA_ACWFSrY85tCeQW-6ffmey8_arthq0tLe_Tnp3swFWlOtjqdwhdOhcIqh0sN1rJk2rEVsvCS3-pyzqaPoZsNSPkEjZlsyUtgkUWFit8';
+logic.token = 'BQBitKmcySh6rJUK1leWI3TMBn-aKDmFpbx1SIMvcu6jCTh7Y2S7eF9UYP8sXwiYXmqgxAtBnD1Zdq10usYdOB_wkjo1EQucR_jfbQuEoCZQ253byT92PX4Hw8EQEjHQcbu6oQEtk0J1YSBUj5wdkYyt1Yh_cBb8rBZDcMBHFJ40';
 
 class App extends Component {
-  state = { artists: [] };
-
-  onSearch = query => {
-    
-    logic.searchArtists(query)
-    .then( artists => {
-      this.setState({artists: artists.map(artist => {
-        return { id: artist.id, text: artist.name }
-        })
-      })
-    }).catch(console.error)
+  state = {
+    artists: [],
+    albums: [],
+    tracks: [],
+    track: undefined
   }
 
+  onSearch = query => {
+    logic.searchArtists(query)
+      .then(artists => {
+        this.setState({
+          artists: artists.map(({ id, name: text }) => ({ id, text })),
+          albums: [],
+          tracks: [],
+          track: undefined
+        })
+      })
+      .catch(console.error)
+  }
+
+  onArtistClick = id => {
+    logic.retrieveAlbumsByArtistId(id)
+      .then(albums => {
+        this.setState({
+          albums: albums.map(({ id, name: text }) => ({ id, text })),
+          tracks: [],
+          track: undefined
+        })
+      })
+      .catch(console.error)
+  }
+
+  onAlbumClick = id => {
+    logic.retrieveTracksByAlbumId(id)
+      .then(tracks => {
+        this.setState({
+          tracks: tracks.map(({ id, name: text }) => ({ id, text })),
+          track: undefined
+        })
+      })
+      .catch(console.error)
+  }
+
+  onTrackClick = id => {
+    logic.retrieveTrackById(id)
+      .then(track => {
+        this.setState({
+          track: {
+            title: track.name,
+            image: track.album.images[0].url,
+            file: track.preview_url,
+            url: track.external_urls.spotify
+          }
+        })
+      })
+      .catch(console.error)
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My Spotify App</h1>
+          <h1 className="App-title">Spotify-App with React</h1>
         </header>
+
+        <h2>Search</h2>
+
         <SearchPanel onSearch={this.onSearch} />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        <ResultList results={(this.state.artists)} />
+        {this.state.artists.length > 0 && <h2>Artists</h2>} 
 
+        <ResultList results={this.state.artists} onItemClick={this.onArtistClick} />
+
+        {this.state.albums.length > 0 && <h2>Artists</h2>}
+
+        <ResultList results={this.state.albums} onItemClick={this.onAlbumClick} />
+
+        {this.state.tracks.length > 0 && <h2>Tracks</h2>}
+
+        <ResultList results={this.state.tracks} onItemClick={this.onTrackClick} />
+
+        {this.state.track && <TrackPlayer track={this.state.track} />}
       </div>
-    )
+    );
   }
 }
 
