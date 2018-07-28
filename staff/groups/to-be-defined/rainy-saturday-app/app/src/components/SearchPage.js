0d85fd88e931bf058c1809fcd04178d3e47ebce1
robert <pulpozero@gmail.com>
2018-07-28 19:49:26 +0200

    search page and form

--- staff/groups/to-be-defined/rainy-saturday-app/app/src/components/SearchPage.js
+++ staff/groups/to-be-defined/rainy-saturday-app/app/src/components/SearchPage.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-
+import SearchForm from './SearchForm'
+import logic from '../logic'
+import ImageDisplayer from "./ImageDisplayer"
 
 class SearchPage extends Component {
 
-render(){
-  return (<h2>SearchPage</h2>)
-}
+  state = {hasImages: false,images:[]}
+
+  doSearch = searchTerm => {
+    this.setState({hasImages: false,images:[]})
+    logic.getMuseumImagesForSearchTerm(searchTerm)
+      .then(results => {
+        this.setState({hasImages: true,images:results})
+        console.log(results)
+      })
+      .catch(console.error)
+  }
 
 
+  render() {
+
+    const {hasImages,images} = this.state
+
+    return (<div><h2>SearchPage</h2>
+        <SearchForm onSearch={this.doSearch}/>
+        {hasImages && <ImageDisplayer data={images}/>}
+      </div>
+    )
+  }
 
 
 }
--- staff/groups/to-be-defined/rainy-saturday-app/app/src/index.js
+++ staff/groups/to-be-defined/rainy-saturday-app/app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { HashRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 registerServiceWorker();
