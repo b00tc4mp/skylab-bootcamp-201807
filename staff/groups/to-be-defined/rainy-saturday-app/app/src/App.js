0f45843729769cc41f327ae6dba3525824a48a34
robert <pulpozero@gmail.com>
2018-07-28 17:24:58 +0200

    add react router

--- staff/groups/to-be-defined/rainy-saturday-app/app/src/App.js
+++ staff/groups/to-be-defined/rainy-saturday-app/app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, withRouter, Link, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
