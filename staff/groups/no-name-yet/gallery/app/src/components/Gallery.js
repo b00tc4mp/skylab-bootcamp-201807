88bf2c373dd92fa4cd69948b9a5ffb0d3c825738
JuanSensio <juansensio03@gmail.com>
2018-07-30 09:20:05 +0200

    first components

---
+++ staff/groups/no-name-yet/gallery/app/src/components/Gallery.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './styles/Gallery.css'
+
+class Gallery extends Component {
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Gallery
