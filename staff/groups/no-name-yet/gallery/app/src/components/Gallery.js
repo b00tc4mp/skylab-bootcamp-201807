cb40afe59ab46ef031238c819bd5c09da331ddc0
JuanSensio <juansensio03@gmail.com>
2018-07-30 16:32:42 +0200

    update

--- staff/groups/no-name-yet/gallery/app/src/components/Gallery.js
+++ staff/groups/no-name-yet/gallery/app/src/components/Gallery.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 
 import './styles/Gallery.css'
 
+import logic from '../logic'
+
 class Gallery extends Component {
 
+    state = {
+        images: []
+    }
+
+    componentDidMount = () => {
+        logic.retrieveImages()
+            .then(res => this.setState({images: res}))
+    }
+
     render() {
+
         return (
-            <div>
+            <div className="gallery">
+                {this.state.images.map( image => <img src={image} /> )}
             </div>
         )
     }
--- staff/groups/no-name-yet/gallery/app/src/components/Main.js
+++ staff/groups/no-name-yet/gallery/app/src/components/Main.js
@@ -1,114 +1,60 @@
 import React, { Component } from 'react'
 
-import cloudinary from 'cloudinary-core'
+import logic from '../logic'
 
 import './styles/Main.css'
 
 class Main extends Component {
 
-    render() {
-        const cl = new cloudinary.Cloudinary({cloud_name: "galleryapp", secure: true});
-
-        return (
-            <div>
-                <div>
-                    <button> Take a picture </button>
-                    <p> or </p>
-                    <input type="file" id="fileinput" />
-                </div>
-            </div>
-        )
+  startWebcam() {
+    const video = document.getElementById("webcam")
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    var facingMode = "environment";
+    var constraints = {
+      audio: false,
+      video: {
+        facingMode: facingMode
+      }
     }
+    navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+      video.srcObject = stream
+    });
+  }
+
+  capture() {
+    const canvas = document.getElementById('canvas')
+    const video = document.getElementById("webcam")
+    const img = document.getElementById('image')
+    canvas.width = video.width
+    canvas.height = video.height
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    img.src = canvas.toDataURL('image/webp');
+    logic.addImage(img)
+  }
+
+  upload = event => {
+    const file = event.target.files[0]
+    logic.addImage(file)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div>
+          <button onClick={this.startWebcam}> Take a picture </button>
+          <p> or </p>
+          <input type="file" id="fileinput" onChange={this.upload} />
+        </div>
+        <div>
+          <video autoPlay playsInline muted id="webcam" width="224" height="224"></video>
+          <canvas id="canvas" style={{ display: "none" }}></canvas>
+          <button onClick={this.capture}>Capture</button>
+          <img id="image" src="" />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Main
-
-/*
-const cloudName = 'Your cloud name';
-const unsignedUploadPreset = 'Your upload preset';
-
-var fileSelect = document.getElementById("fileSelect"),
-  fileElem = document.getElementById("fileElem");
-
-fileSelect.addEventListener("click", function(e) {
-  if (fileElem) {
-    fileElem.click();
-  }
-  e.preventDefault(); // prevent navigation to "#"
-}, false);
-
-// ************************ Drag and drop ***************** //
-function dragenter(e) {
-  e.stopPropagation();
-  e.preventDefault();
-}
-
-function dragover(e) {
-  e.stopPropagation();
-  e.preventDefault();
-}
-
-dropbox = document.getElementById("dropbox");
-dropbox.addEventListener("dragenter", dragenter, false);
-dropbox.addEventListener("dragover", dragover, false);
-dropbox.addEventListener("drop", drop, false);
-
-function drop(e) {
-  e.stopPropagation();
-  e.preventDefault();
-
-  var dt = e.dataTransfer;
-  var files = dt.files;
-
-  handleFiles(files);
-}
-
-// *********** Upload file to Cloudinary ******************** //
-function uploadFile(file) {
-  var url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
-  var xhr = new XMLHttpRequest();
-  var fd = new FormData();
-  xhr.open('POST', url, true);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  // Reset the upload progress bar
-   document.getElementById('progress').style.width = 0;
-  
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function(e) {
-    var progress = Math.round((e.loaded * 100.0) / e.total);
-    document.getElementById('progress').style.width = progress + "%";
-
-    console.log(`fileuploadprogress data.loaded: ${e.loaded},
-  data.total: ${e.total}`);
-  });
-
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // File uploaded successfully
-      var response = JSON.parse(xhr.responseText);
-      // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
-      var url = response.secure_url;
-      // Create a thumbnail of the uploaded image, with 150px width
-      var tokens = url.split('/');
-      tokens.splice(-2, 0, 'w_150,c_scale');
-      var img = new Image(); // HTML5 Constructor
-      img.src = tokens.join('/');
-      img.alt = response.public_id;
-      document.getElementById('gallery').appendChild(img);
-    }
-  };
-
-  fd.append('upload_preset', unsignedUploadPreset);
-  fd.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
-  fd.append('file', file);
-  xhr.send(fd);
-}
-
-// *********** Handle selected files ******************** //
-var handleFiles = function(files) {
-  for (var i = 0; i < files.length; i++) {
-    uploadFile(files[i]); // call the function to upload the file
-  }
-};
-*/
