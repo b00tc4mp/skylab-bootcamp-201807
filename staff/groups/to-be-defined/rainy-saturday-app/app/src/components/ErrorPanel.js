b8a78be5a3c97be5d9d28d06671058ae4577cdb7
gbaste <gbfabre@gmail.com>
2018-07-31 12:37:17 +0200

    add changes errorpanel

--- staff/groups/to-be-defined/rainy-saturday-app/app/src/components/ErrorPanel.js
+++ staff/groups/to-be-defined/rainy-saturday-app/app/src/components/ErrorPanel.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Alert } from 'reactstrap';
+import PropTypes from 'prop-types'
 
 const ErrorPanel = (props) => {
+  let color = props.color || "danger"
   return (
     <div>
-      <Alert color="danger">
+      <Alert color={color}>
         {props.message}
       </Alert>
     </div>
