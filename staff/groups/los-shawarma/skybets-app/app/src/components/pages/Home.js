89b03517a15a9db03061797a0363a148a7b6ee99
laia89 <laiacy234@gmail.com>
2018-08-01 18:47:39 +0200

    first pagination version of flightsResults

--- staff/groups/los-shawarma/skybets-app/app/src/components/pages/Home.js
+++ staff/groups/los-shawarma/skybets-app/app/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import SearchFlights from '../cards/SearchFlights';
+import SearchFlights from '../cards/SearchFlights'
 import logic from '../../logic'
+import FlightResults from '../cards/FlightResults'
 
 class Home extends Component {
 
@@ -8,7 +9,8 @@ class Home extends Component {
         selectedFrom : null,
         selectedTo : null,
         dateFrom : null,
-        dateTo : null
+        dateTo : null,
+        flights: []
     }
     
     parseIata = iata => iata.substring(0, 3)
@@ -36,16 +38,46 @@ class Home extends Component {
         const inputDateTo = this.parseDate(dateTo)
 
         logic._callKiwiApi(fromIata, toIata, inputDateFrom, inputDateTo)
+        .then(flights => {
+            var flights = this.filterFlightsData(flights);
+            this.setState({flights: flights})
+        })
+        .catch()
+    }
+
+    filterFlightsData = (flights) => {
+
+        const filteredFlights = []
+        flights.forEach(flight => {
+            const flightFrom = flight.route[0]
+            const flightTo = flight.route[1]
+            const filteredFlight= {
+                price: flight.conversion.EUR,
+                dateFrom: flightFrom.aTimeUTC,
+                dateTo: flightTo.aTimeUTC,
+                cityFrom: flightFrom.cityFrom,
+                cityTo: flightFrom.cityTo,
+                flyFrom: flightFrom.flyFrom,
+                flyTo: flightFrom.flyTo,
+                link: flight.deep_link
+            }
+            filteredFlights.push(filteredFlight);
+        })
+
+        return filteredFlights
     }
     
 
     
     render() {
 
+        const {flights} = this.state
+
         return(
             <main>
                 <h1>Home Page</h1>
-                <SearchFlights getDataInputsProp={ this.getDataInputs } />            
+                <SearchFlights getDataInputsProp={ this.getDataInputs } /> 
+                {flights.length > 0 && <FlightResults flightsProp={flights}/>}         
             </main>
         )
     }
