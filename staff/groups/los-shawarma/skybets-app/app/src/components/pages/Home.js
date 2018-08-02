d161a14f6490c16f67e80a7d46f2cb2187721ae2
Pau <idea.barcelona@gmail.com>
2018-08-02 17:15:44 +0200

    fav home & card 0.1

--- staff/groups/los-shawarma/skybets-app/app/src/components/pages/Home.js
+++ staff/groups/los-shawarma/skybets-app/app/src/components/pages/Home.js
@@ -19,14 +19,24 @@ class Home extends Component {
         bets: [],
         currentPrice: null,
         currentOdds: null,
+        currentBet: null,
+        currentFlight: null
+
     }
 
     keepCurrentPrice = ({ price: currentPrice }) => {
-        debugger;
+        
         this.setState({ currentPrice })
     }
 
     keepCurrentOdds = ({ odds: currentOdds }) => this.setState({ currentOdds })
+    keepCurrentFlight = currentFlight => {
+        
+        this.setState({ currentFlight })
+    }
+    
+    
+    keepCurrentBet = currentBet => this.setState({ currentBet })
     
     parseIata = iata => iata.substring(0, 3)
 
@@ -63,15 +73,17 @@ class Home extends Component {
         logic._callKiwiApi(fromIata, toIata, inputDateFrom, inputDateTo)
          .then(flights => {
              const filteredFlights = this.filterFlightsData(flights);
-             debugger;
+            
              this.setState({flights: filteredFlights})
-             this.setState({currentPrice: filteredFlights[0].price})
+             //this.setState({currentPrice: filteredFlights[0].price})
+             this.setState({currentFlight: filteredFlights[0]})
 
          })
          .then(logic._callBetsApi)
          .then(bets => {
              const filteredBets = this.filterBetsData(bets);
              this.setState({bets: filteredBets})
+             this.setState({currentBet: filteredBets[0]})
          })
          .catch()
     }
@@ -109,8 +121,8 @@ class Home extends Component {
 
     render() {
 
-        const {flights, bets, currentPrice, currentOdds} = this.state
-        debugger;
+        const {flights, bets, currentPrice, currentOdds, currentBet, currentFlight} = this.state
+        
         return(
             <main>
                 <h1>Home Page</h1>
@@ -119,17 +131,31 @@ class Home extends Component {
                 <ResultsSlider 
                     resultsProp={ flights } 
                     titleProps={ 'Flights' }
-                    onPageChangedProp={ this.keepCurrentPrice }
+                    //onPageChangedProp={ this.keepCurrentPrice }
+                    onPageChangedProp={ this.keepCurrentFlight }
                     render={ currentFlight => <FlightCard flightsProp={currentFlight}/> }
                 />
                 <ResultsSlider 
                     resultsProp={ bets } 
                     titleProps={ 'Bets' }
-                    onPageChangedProp={ this.keepCurrentOdds }
+                    //onPageChangedProp={ this.keepCurrentOdds }
+                    onPageChangedProp={ this.keepCurrentBet }
                     render={ currentBet => <BetCard betsProp={currentBet}/> }
                 />
                 <BetPriceCard  currentPriceProp={currentPrice} currentOddsProp={currentOdds}/>
-                <FavCard currentPriceProp={currentPrice} currentOddsProp={currentOdds}/>
+                {/*<BetPriceCard  currentPriceProp={currentFlight[0].price} currentOddsProp={currentBet.odds}/>*/}
+                
+
+                
+                {(currentFlight && currentBet) && <FavCard 
+                    currentPriceProp={currentPrice} 
+                    currentOddsProp={currentOdds}
+                    currentFlightProp={currentFlight}
+                    currentBetProp={currentBet}
+                />}
+
+
+
             </main>
         )
     }
