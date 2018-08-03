3249a0106ab74308d817b7a40fa93de82f7dd2c1
laia89 <laiacy234@gmail.com>
2018-08-03 15:30:07 +0200

    fix myfavs when empty

--- staff/groups/los-shawarma/skybets-app/app/src/components/pages/MyFavs.js
+++ staff/groups/los-shawarma/skybets-app/app/src/components/pages/MyFavs.js
@@ -2,21 +2,30 @@ import React from 'react'
 import FavCard from '../cards/FavCard'
 
 const MyFavs = props => {
-   
-           return(
-            <section>
-                {props.favsProp.map((fav, index) => {
-                    return(<div key={index}>
-                        <FavCard 
-                            currentPriceProp={fav.flight.price} 
-                            currentOddsProp={fav.bet.odds}
-                            currentFlightProp={fav.flight}
-                            currentBetProp={fav.bet}                        
-                        /> 
-                    </div>)
-                    })}
-            </section>
-        )
+
+        if (props && props.favsProp && props.favsProp[0] && props.favsProp[0].flight) {
+            return(
+                <section>
+                    {props.favsProp.map((fav, index) => {
+                        return(<div key={index}>
+                            <FavCard 
+                                currentPriceProp={fav.flight.price} 
+                                currentOddsProp={fav.bet.odds}
+                                currentFlightProp={fav.flight}
+                                currentBetProp={fav.bet}                        
+                            /> 
+                        </div>)
+                        })}
+                </section>
+            )
+        } else {
+            return(
+                <div>
+                    <span>Your favourites list is empty</span>
+                </div>
+            )
+        }
+           
     
     
 }
