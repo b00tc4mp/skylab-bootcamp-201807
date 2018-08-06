49f7cbd6c903fd7e7b87a1d115ed3547a50729b5
manuelbarzi <manuelbarzi@gmail.com>
2018-08-06 17:30:38 +0200

    recover file

--- staff/manuel-barzi/strings/scripts/to-camel-case.js
+++ staff/manuel-barzi/strings/scripts/to-camel-case.js
@@ -1,9 +1,3 @@
 function toCamelCase(string) {
     // TODO return string into camelCase
-    var words = splitToWords(string.toLowerCase())
-    var newString = [];
-    for (var i = 1; i < words.length; i++) {
-        words[1] = words[i][0].toUpperCase() + words[1].slice(1);
-    }
-    return words.join("");
 }
