df00f193ba6abb1d8b1214b13b9b9024671762b0
Maider <info@maiderhernandorena.com>
2018-08-08 13:52:40 +0200

    transfer-line

---
+++ staff/maider-hernandorena/transfer-file/server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const fs = require('fs')
+
+const { argv: [, , port] } = process
+
+http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        const ws = fs.createWriteStream('file.data')
+
+        req.pipe(ws)
+
+        res.on('end', () => res.end('ok, filed received!'))
+    }
+
+}).listen(port, () => console.log(`transfer-file server up and running on port ${port}`))
