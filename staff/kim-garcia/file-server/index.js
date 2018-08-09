49bd63cc992d4284fc8133edd7529f9eee5b79ed
Kimbali <kimgarcianton@hotmail.com>
2018-08-09 19:32:59 +0200

    logou and header

--- staff/kim-garcia/file-server/index.js
+++ staff/kim-garcia/file-server/index.js
@@ -2,82 +2,201 @@ const express = require('express')
 const fs = require('fs')
 const fileUpload = require('express-fileupload')
 const package = require('./package.json')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const bodyParser = require('body-parser')
+const logic = require('./logic')
 
 const { argv: [, , port] } = process
 
-const app = express();
+const app = express()
 
-// 
-app.use(fileUpload());
+app.use(fileUpload())
+app.use(express.static('public'))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        //secure: true // WARN! in case requiring it to work in https only
+    },
+    store: new FileStore
+}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-//statis es un midlewire que permite acceder a una carpeta al nivel d index
-app.use(express.static('public')); // para añadir styles.css, luego lo añadimos en head
-
-app.get('/helloworld', (req, res) => {
-    res.send(`<html>
-    <head>
-        <title>hola mundo</title>
-        <link rel="stylesheet" href"styles.css"></link>
-    </head>
-    <body>
-        <h1>hello world!</h1>
-    </body>
-</html>`)
-})
-
-app.get('/files', (req, res) => {
-    const files = fs.readdirSync('files')
-
+app.get('/', (req, res) => {
     res.send(`<html>
     <head>
         <title>files</title>
     </head>
     <body>
-        <ul>
-            ${files.map(file => `<li><a href="/download/${file}"> ${file}</a> <button> <a href="/delete/${file}"> X</a></button></li>`).join('')}
-        </ul>
-
-        <form action="/files" method="post" encType="multipart/form-data">
-            <input type="file" name="upload">
-            <button>upload</button>
-        </form>
+        <h1>welcome to files!</h1>
+        please, <a href="/register">register</a> or <a href="/login">login</a>.
     </body>
 </html>`)
 })
 
-app.get('/delete/:file', (req, res) => {
-    const fileDelete = 'files/' + req.params.file; // selecciona arxiu
-    console.log(req.params)
-    fs.unlinkSync(fileDelete)  //delate
-    res.redirect('/files') // recargues tot , torna a fer el get
-})
+app.get('/files', (req, res) => {
+    const { session } = req
 
-app.get('/download/:file', (req, res) => {
-    const filesDown = 'files/' + req.params.file;
+    try {
+        if (logic.isLoggedIn(session.username)) {
+            const files = fs.readdirSync('files')
 
-    res.download(filesDown);
+            res.send(`<html>
+    <head>
+        <title>files</title>
+        <link href="styles.css" rel="stylesheet">
+        <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
+    </head>
+    <body>
+        <header>
+            FILES FILES FILES <a href="/logout">Logout</a>
+        </header>
+        <ul>
+            ${files.map(file => `<li>${file}</li>`).join('')}
+        </ul>
+
+        <form action="/files" method="post" encType="multipart/form-data">
+            <input type="file" name="upload">
+            <button type="submit">upload</button>
+        </form>
+    </body>
+</html>`)
+
+        } else {
+            res.redirect('/')
+        }
+    } catch ({ message }) {
+        res.redirect('/')
+    }
 })
 
 app.post('/files', (req, res) => {
-    console.log(req)
-    console.log(res)
-    const { files: { upload } } = res
-    const { }
+    const { files: { upload }, session } = req
 
-    if(upload)
+    if (upload) {
+        session.error = ''
+
         upload.mv(`files/${upload.name}`, function (err) {
             if (err)
                 return res.status(500).send(err)
 
             res.redirect('/files')
         })
-    else 
-        res 
+        // else
+        //     res.redirect('/files?error=EMPTY_FILE')
+    } else {
+        session.error = 'EMPTY_FILE'
+
+        res.redirect('/files')
+    }
 })
 
+app.get('/register', (req, res) => {
+    const { session: { error } } = req
+
+    res.send(`<html>
+    <head>
+        <title>files</title>
+        <link href="styles.css" rel="stylesheet">
+        <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
+    </head>
+    <body>
+        <h1>register</h1>
+
+        <form action="/register" method="post">
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <button type="submit">register</button>
+        </form>
+
+        <a href="/login" >Go to login</a>
 
 
+        ${error ? `<h2 class="error">${error}</h2>` : ''}
+    </body>
+</html>`)
+})
+
+app.post('/register', (req, res) => {
+    const { session, body: { username, password } } = req
+
+    try {
+        logic.register(username, password)
+
+        res.send(`<html>
+            <head>
+                <title>files</title>
+                <link href="styles.css" rel="stylesheet">
+                <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
+            </head>
+            <body>
+                ok, user ${username} successfully registered, now you can go to <a href="/login">login</a>
+            </body>
+        </html>`)
+    } catch ({ message }) {
+        session.error = message
+
+        res.redirect('/register')
+    }
+})
+
+app.get('/login', (req, res) => {
+    const { session: { error } } = req
+
+    res.send(`<html>
+    <head>
+        <title>files</title>
+        <link href="styles.css" rel="stylesheet">
+        <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
+    </head>
+    <body>
+        <h1>login</h1>
+
+        <form action="/login" method="post">
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <button type="submit">login</button>
+        </form>
+
+        <a href="/register" >Go to register</a>
+
+        ${error ? `<h2 class="error">${error}</h2>` : ''}        
+    </body>
+</html>`)
+})
+
+app.post('/login', (req, res) => {
+    const { session, body: { username, password } } = req
+
+    try {
+        logic.login(username, password)
+
+        session.username = username
+
+        res.redirect('/files')
+    } catch ({ message }) {
+        session.error = message
+
+        res.redirect('/login')
+    }
+})
+
+app.get('/logout', (req, res) => {
+    // const { session, body: { username, password } } = req
+    
+    try {
+        username = req.session.username
+        logic.logout(username)
+        res.redirect('/')
+        
+    } catch ({ message }) {
+        session.error = message
+
+        res.redirect('/login')
+    }
+})
 
 app.listen(port, () => console.log(`${package.name} ${package.version} up and running on port ${port}`))
-//file-server 1.0.0 up and running on port 8080  
-//|| 8080
