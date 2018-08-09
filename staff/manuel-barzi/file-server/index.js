b17bd5a68087c5049d603c6b4dfe4bc01a33307e
manuelbarzi <manuelbarzi@gmail.com>
2018-08-09 10:56:17 +0200

    update file-server

--- staff/manuel-barzi/file-server/index.js
+++ staff/manuel-barzi/file-server/index.js
@@ -9,6 +9,14 @@ const app = express()
 
 app.use(fileUpload())
 
+app.use(express.static('public'))
+
+const errors = {
+    EMPTY_FILE: 'cannot upload empty files'
+}
+
+const sessions = {}
+
 app.get('/helloworld', (req, res) => {
     res.send(`<html>
     <head>
@@ -21,11 +29,20 @@ app.get('/helloworld', (req, res) => {
 })
 
 app.get('/files', (req, res) => {
+    //const { query: { error } } = req
     const files = fs.readdirSync('files')
+
+    let errorMessage
+
+    const session = getSession(req)
+
+    if (session.error) errorMessage = errors[session.error]
 
     res.send(`<html>
     <head>
         <title>files</title>
+        <link href="styles.css" rel="stylesheet">
+        <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
     </head>
     <body>
         <ul>
@@ -36,6 +53,8 @@ app.get('/files', (req, res) => {
             <input type="file" name="upload">
             <button>upload</button>
         </form>
+
+        ${errorMessage ? `<h2 class="error">${errorMessage}</h2>` : ''}
     </body>
 </html>`)
 })
@@ -43,12 +62,36 @@ app.get('/files', (req, res) => {
 app.post('/files', (req, res) => {
     const { files: { upload } } = req
 
-    upload.mv(`files/${upload.name}`, function (err) {
-        if (err)
-            return res.status(500).send(err)
+    const session = getSession(req)
+
+    if (upload) {
+        session.error = ''
+
+        upload.mv(`files/${upload.name}`, function (err) {
+            if (err)
+                return res.status(500).send(err)
+
+            res.redirect('/files')
+        })
+        // else
+        //     res.redirect('/files?error=EMPTY_FILE')
+    } else {
+        session.error = 'EMPTY_FILE'
 
         res.redirect('/files')
-    });
+    }
 })
+
+function getSession(req) {
+    const sessionId = req.connection.remoteAddress + '-' + req.headers['user-agent']
+
+    let session = sessions[sessionId]
+
+    if (!session) sessions[sessionId] = session = {}
+
+    console.log(JSON.stringify(sessions, null, 4))
+
+    return session
+}
 
 app.listen(port, () => console.log(`${package.name} ${package.version} up and running on port ${port}`))
