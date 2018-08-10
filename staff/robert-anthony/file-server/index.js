73b02b24aacefb0baad9c1093cb569000ea831f7
robert <pulpozero@gmail.com>
2018-08-10 11:03:15 +0200

    fileserver

--- staff/robert-anthony/file-server/index.js
+++ staff/robert-anthony/file-server/index.js
@@ -1,114 +1,96 @@
 const express = require('express')
 const fs = require('fs')
 const fileUpload = require('express-fileupload')
-const package = require('./package.json')
+const pkg = require('./package.json')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 const bodyParser = require('body-parser')
 const logic = require('./logic')
 
-const { argv: [, , port] } = process
+const {argv: [, , port]} = process
 
 const app = express()
 
 app.use(fileUpload())
 app.use(express.static('public'))
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'pug')
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 24 * 60 * 60 * 1000,
-        //secure: true // WARN! in case requiring it to work in https only
-    },
-    store: new FileStore
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    //secure: true // WARN! in case requiring it to work in https only
+  },
+  store: new FileStore
 }))
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
-const errors = {
-    EMPTY_FILE: 'cannot upload empty files'
-}
-
-app.get('/helloworld', (req, res) => {
-    res.send(`<html>
-    <head>
-        <title>hola mundo</title>
-    </head>
-    <body>
-        <h1>hello world!</h1>
-    </body>
-</html>`)
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.get('/files', (req, res) => {
-    //const { query: { error } } = req
-    const files = fs.readdirSync('files')
-
-    let errorMessage
-
-    const { session } = req
-
+  const {session} = req
+try {
     if (logic.isLoggedIn(session.username)) {
+     /* const userDirectory = `files/${session.username}`
 
-        if (session.error) errorMessage = errors[session.error]
+      if (!fs.existsSync(userDirectory))
+        try {
+          fs.mkdirSync(userDirectory)
+        } catch (err) {
+          throw err;
+        }*/
+      const files = fs.readdirSync('files')
 
-        res.send(`<html>
-    <head>
-        <title>files</title>
-        <link href="styles.css" rel="stylesheet">
-        <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
-    </head>
-    <body>
-        <ul>
-            ${files.map(file => `<li>${file}</li>`).join('')}
-        </ul>
-
-        <form action="/files" method="post" encType="multipart/form-data">
-            <input type="file" name="upload">
-            <button type="submit">upload</button>
-        </form>
-
-        ${errorMessage ? `<h2 class="error">${errorMessage}</h2>` : ''}
-    </body>
-</html>`)
+      res.render('files',{files:files})
 
     } else {
-        res.send(`<html>
-    <head>
-        <title>files</title>
-        <link href="styles.css" rel="stylesheet">
-        <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
-    </head>
-    <body>
-        please, proceed to login to access files
-    </body>
-</html>`)
+      res.redirect('/')
     }
+  } catch ({message}) {
+    console.error("error",message)
+    res.redirect('/')
+  }
 })
 
 app.post('/files', (req, res) => {
-    const { files: { upload }, session } = req
+  const {files: {upload}, session} = req
 
-    if (upload) {
-        session.error = ''
+  if (upload) {
+    session.error = ''
 
-        upload.mv(`files/${upload.name}`, function (err) {
-            if (err)
-                return res.status(500).send(err)
+  /*  const userDirectory = `files/${session.username}`
 
-            res.redirect('/files')
-        })
-        // else
-        //     res.redirect('/files?error=EMPTY_FILE')
-    } else {
-        session.error = 'EMPTY_FILE'
+    if (!fs.existsSync(userDirectory))
+      try {
+        fs.mkdirSync(userDirectory)
+      } catch (err) {
+        throw err;
+      }*/
 
-        res.redirect('/files')
-    }
+    //upload.mv(`files/${userDirectory}/${upload.name}`, function (err) {
+    upload.mv(`files/${upload.name}`, function (err) {
+      if (err)
+        return res.status(500).send(err)
+
+      res.redirect('/files')
+    })
+    // else
+    //     res.redirect('/files?error=EMPTY_FILE')
+  } else {
+    session.error = 'EMPTY_FILE'
+
+    res.redirect('/files')
+  }
 })
 
 app.get('/register', (req, res) => {
-    res.send(`<html>
+  const {session: {error}} = req
+
+  res.send(`<html>
     <head>
         <title>files</title>
         <link href="styles.css" rel="stylesheet">
@@ -122,42 +104,39 @@ app.get('/register', (req, res) => {
             <input type="password" name="password">
             <button type="submit">register</button>
         </form>
+
+        ${error ? `<h2 class="error">${error}</h2>` : ''}
     </body>
 </html>`)
 })
 
 app.post('/register', (req, res) => {
-    const { body: { username, password } } = req
+  const {session, body: {username, password}} = req
 
-    try {
-        logic.register(username, password)
+  try {
+    logic.register(username, password)
 
-        res.send(`<html>
+    res.send(`<html>
             <head>
                 <title>files</title>
                 <link href="styles.css" rel="stylesheet">
                 <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
             </head>
             <body>
-                <h1>ok, user ${username} successfully registered</h1>
+                ok, user ${username} successfully registered, now you can go to <a href="/login">login</a>
             </body>
         </html>`)
-    } catch ({message}) {
-        res.send(`<html>
-            <head>
-                <title>files</title>
-                <link href="styles.css" rel="stylesheet">
-                <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
-            </head>
-            <body>
-                <h1>Error: ${message}</h1>
-            </body>
-        </html>`)
-    }
+  } catch ({message}) {
+    session.error = message
+
+    res.redirect('/register')
+  }
 })
 
 app.get('/login', (req, res) => {
-    res.send(`<html>
+  const {session: {error}} = req
+
+  res.send(`<html>
     <head>
         <title>files</title>
         <link href="styles.css" rel="stylesheet">
@@ -171,27 +150,26 @@ app.get('/login', (req, res) => {
             <input type="password" name="password">
             <button type="submit">login</button>
         </form>
+
+        ${error ? `<h2 class="error">${error}</h2>` : ''}        
     </body>
 </html>`)
 })
 
 app.post('/login', (req, res) => {
-    const { body: { username, password }, session } = req
+  const {session, body: {username, password}} = req
 
+  try {
     logic.login(username, password)
 
     session.username = username
 
-    res.send(`<html>
-    <head>
-        <title>files</title>
-        <link href="styles.css" rel="stylesheet">
-        <link href="skylab-icon.png" type="image/png" rel="Shortcut Icon">
-    </head>
-    <body>
-        <h1>${logic.isLoggedIn(session.username) ? `ok, user ${username} successfully logged in` : `ko, user ${username} failed to log in`}</h1>
-    </body>
-</html>`)
+    res.redirect('/files')
+  } catch ({message}) {
+    session.error = message
+
+    res.redirect('/login')
+  }
 })
 
-app.listen(port, () => console.log(`${package.name} ${package.version} up and running on port ${port}`))
+app.listen(port, () => console.log(`${pkg.name} ${pkg.version} up and running on port ${port}`))
