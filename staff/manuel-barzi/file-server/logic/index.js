42d4cb7cd57cb16d778138d69f8a0c3b7c82ec23
manuelbarzi <manuelbarzi@gmail.com>
2018-08-10 13:31:39 +0200

    update file-server

--- staff/manuel-barzi/file-server/logic/index.js
+++ staff/manuel-barzi/file-server/logic/index.js
@@ -5,12 +5,19 @@ const fs = require('fs')
 const logic = {
     _users: {},
 
+    // TODO test!
     _persist() {
         fs.writeFileSync('data/users.json', JSON.stringify(this._users))
     },
 
     _validateStringField(fieldName, fieldValue) {
         if (typeof fieldValue !== 'string' || !fieldValue.length) throw new Error(`invalid ${fieldName}`)
+    },
+
+    _validateUserExists(username) {
+        const user = this._users[username]
+
+        if (!user) throw new Error(`user ${username} does not exist`)
     },
 
     register(username, password) {
@@ -32,9 +39,9 @@ const logic = {
         this._validateStringField('username', username)
         this._validateStringField('password', password)
 
-        const user = this._users[username]
+        this._validateUserExists(username)
 
-        if (!user) throw new Error(`user ${username} does not exist`)
+        const user = this._users[username]
 
         if (user.password === password) {
             user.loggedIn = true
@@ -46,10 +53,9 @@ const logic = {
     isLoggedIn(username) {
         this._validateStringField('username', username)
 
+        this._validateUserExists(username)
 
         const user = this._users[username]
-
-        if (!user) throw new Error(`user ${username} does not exist`)
 
         return user.loggedIn
     },
@@ -57,13 +63,56 @@ const logic = {
     logout(username) {
         this._validateStringField('username', username)
 
-        const user = this._users[username]
+        this._validateUserExists(username)
 
-        if (!user) throw new Error(`user ${username} does not exist`)
+        const user = this._users[username]
 
         user.loggedIn = false
 
         this._persist()
+    },
+
+    listFiles(username) {
+        this._validateStringField('username', username)
+
+        this._validateUserExists(username)
+
+        return fs.readdirSync(`files/${username}`)
+    },
+
+    // DEPRECATED
+    // TODO test!
+    // getFilesFolder(username) {
+    //     return `files/${username}`
+    // }
+
+    saveFile(username, filename, buffer) {
+        this._validateStringField('username', username)
+        this._validateStringField('filename', filename)
+
+        if (typeof buffer === 'undefined' || /*!(buffer instanceof Buffer)*/ !Buffer.isBuffer(buffer)) throw new Error('invalid buffer')
+
+        this._validateUserExists(username)
+
+        fs.writeFileSync(`files/${username}/${filename}`, buffer)
+    },
+
+    getFilePath(username, file) {
+        this._validateStringField('username', username)
+        this._validateStringField('file', file)
+
+        this._validateUserExists(username)
+
+        return `files/${username}/${file}`
+    },
+
+    removeFile(username, file) {
+        this._validateStringField('username', username)
+        this._validateStringField('file', file)
+
+        this._validateUserExists(username)
+
+        fs.unlinkSync(`files/${username}/${file}`)
     }
 }
 
