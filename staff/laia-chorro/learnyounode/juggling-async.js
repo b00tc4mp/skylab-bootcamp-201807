90cb1223c99dbfb3f37b76847f175c8d9751998c
laia89 <laiacy234@gmail.com>
2018-08-11 12:56:24 +0200

    clean code

--- staff/laia-chorro/learnyounode/juggling-async.js
+++ staff/laia-chorro/learnyounode/juggling-async.js
@@ -9,118 +9,25 @@
   command-line arguments. */
 
 
-var bl = require('bl') ;
-var http = require('http');
+const bl = require('bl') ;
+const http = require('http');
 
-var url1 = process.argv[2];
-var url2 = process.argv[3];
-var url3 = process.argv[4];
+const { argv: [, , ...urls ] } = process
 
+let count = 0
 
-console.log('url1 ' + url1)
-console.log('url2 ' + url2)
-console.log('url3 ' + url3 + '\n')
-
-
-//node inspect juggling-async.js "http://localhost:46540" "http://localhost:37390" "http://localhost:40368"
-
-
-
-function httpCollect(url, endCallback) {
-    http.get(url, function(response){
-        response.pipe(bl(function (err, data) {
-            if (err) throw err
-            console.log('primerrrrr ' + url)
-            console.log(data.toString() + '\n')
-         }))
-
-         console.log('endCallback ' + endCallback + '\n')
-
-         if (endCallback) {
-            response.on("end", function() {
-                console.log('endddddd ' + url + '\n')
-                endCallback();
-             }) 
-         }
-    })
-}
-
-httpCollect(url1, httpCollect(url2, console.log('HOLAAAAAAAAAAA')))
-
-
-
-//httpCollect(url1, httpCollect(url2, httpCollect(url3, null)))
-
-
-
-
-
-
-/*function secondAnswer(){
-    http.get(url2, function(response){
-        response.pipe(bl(function (err, data) {
+const httpCollect = url => {
+    http.get(url, response => {
+        response.pipe(bl( (err, data) => {
             if (err) throw err
             console.log(data.toString())
-         }))
-    })
-}
+        }))
 
-function thirdAnswer(){
-    http.get(url2, function(response){
-        response.pipe(bl(function (err, data) {
-            if (err) throw err
-            console.log(data.toString())
-         }))
-    })
-}
+        if (count < urls.length -1) {
+            count++
+            response.on('end', () => { httpCollect(urls[count]); }) 
+        } 
+    })       
+} 
 
-*/
-
-
-/*http.get(url1, function(response){
-    response.pipe(bl(function (err, data) {
-        if (err) throw err
-debugger;
-        console.log(data.toString())
-     }))
-     response.on("end", function() {
-        http.get(url2, function(response){
-            response.pipe(bl(function (err, data) {
-                if (err) throw err
-                console.log(data.toString())
-             }))
-        })
-     }) 
-})*/
-
-/*
-http.get(url1, function(response){
-    response.pipe(bl(function (err, data) {
-        if (err) throw err
-        console.log(data.toString())
-     }))
-     response.on("end", function() {
-        http.get(url2, function(response){
-            response.pipe(bl(function (err, data) {
-                if (err) throw err
-                console.log(data.toString())
-             }))
-
-             response.on("end", function() {
-                http.get(url3, function(response){
-                    response.pipe(bl(function (err, data) {
-                        if (err) throw err
-                        console.log(data.toString())
-                     }))
-                })
-             }) 
-        })
-     }) 
-})*/
-
-
-
-
-
-
-
+httpCollect(urls[count])
