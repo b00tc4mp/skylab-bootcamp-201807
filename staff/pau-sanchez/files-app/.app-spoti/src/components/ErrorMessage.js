314f2b4ce4a33c7b8156bdb5f69834bcd43e8990
Pau <idea.barcelona@gmail.com>
2018-08-15 17:58:24 +0200

    files app api 0.0

--- staff/pau-sanchez/files-app/.app-spoti/src/components/ErrorMessage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Feedback.css'
-
-function Feedback(props) {
-    return <h4 className="error">{props.message}</h4>
-}
-
-export default Feedback
--- staff/pau-sanchez/files-app/.app-spoti/src/components/Feedback.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Feedback.css'
-
-function Feedback(props) {
-    return <h4 className={props.info ? "info" : "error"}>{props.info || props.error}</h4>
-}
-
-export default Feedback
