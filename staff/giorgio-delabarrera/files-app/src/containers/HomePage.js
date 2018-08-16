82a6bfebdd5f5e1d40244a15d25ea00d55f574d7
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-08-16 15:35:36 +0200

    api file with axios

---
+++ staff/giorgio-delabarrera/files-app/src/containers/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+class HomePage extends Component {
+
+  render() {
+    return (
+      <div>
+        HOMEPAGE
+      </div>
+    )
+  }
+}
+
+export default withRouter(HomePage)
