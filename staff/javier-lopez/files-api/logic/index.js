363098c317c13a61617889b85bb2e39590a74e34
Javi <javierlopezblasco@gmail.com>
2018-08-16 17:59:33 +0200

    starting mongo

--- staff/javier-lopez/files-api/logic/index.js
+++ staff/javier-lopez/files-api/logic/index.js
@@ -4,42 +4,50 @@ const fs = require('fs')
 
 if (!fs.existsSync('data')) {
     fs.mkdirSync('data')
-
-    fs.writeFileSync('data/users.json', '{}')
 }
 
 const logic = {
-    _users: {},
-
-    // TODO test!
-    _persist() {
-        fs.writeFileSync('data/users.json', JSON.stringify(this._users))
-    },
+    _users: null,
 
     _validateStringField(fieldName, fieldValue) {
-        if (typeof fieldValue !== 'string' || !fieldValue.length) throw new Error(`invalid ${fieldName}`)
+        if (typeof fieldValue !== 'string' || !fieldValue.length) throw new LogicError(`invalid ${fieldName}`)
     },
 
     _validateUserExists(username) {
-        const user = this._users[username]
-
-        if (!user) throw new Error(`user ${username} does not exist`)
+        return this._users.findOne({ username })
+            .then(user => {
+                if (!user) throw new LogicError(`user ${username} does not exist`)
+            })
     },
 
     register(username, password) {
-        this._validateStringField('username', username)
-        this._validateStringField('password', password)
+        return Promise.resolve()
+            .then(() => {
+                this._validateStringField('username', username)
+                this._validateStringField('password', password)
 
-        const user = this._users[username]
+                return this._users.findOne({ username })
+            })
+            .then(user => {
+                if (user) throw new LogicError(`user ${username} already exists`)
 
-        if (user) throw new Error(`user ${username} already exists`)
+                const _user = { username, password }
 
-        this._users[username] = { password }
+                return this._users.insertOne(_user)
+            })
+            .then(() =>
+                new Promise((resolve, reject) => {
+                    fs.mkdir(`data/${username}`, err => {
+                        if (err) return reject(err)
 
-        fs.mkdirSync(`data/${username}`)
-        fs.mkdirSync(`data/${username}/files`)
+                        fs.mkdir(`data/${username}/files`, err => {
+                            if (err) return reject(err)
 
-        this._persist()
+                            resolve()
+                        })
+                    })
+                })
+            )
     },
 
     authenticate(username, password) {
@@ -50,7 +58,21 @@ const logic = {
 
         const user = this._users[username]
 
-        if (user.password !== password) throw new Error('wrong credentials')
+        if (user.password !== password) throw new LogicError('wrong credentials')
+    },
+
+    updatePassword(username, password, newPassword) {
+        this.authenticate(username, password)
+
+        this._validateStringField('new password', newPassword)
+
+        if (password === newPassword) throw new LogicError('new password cannot be same as current password')
+
+        const user = this._users[username]
+
+        user.password = newPassword
+
+        this._persist()
     },
 
     listFiles(username) {
@@ -71,7 +93,7 @@ const logic = {
         this._validateStringField('username', username)
         this._validateStringField('filename', filename)
 
-        if (typeof buffer === 'undefined' || /*!(buffer instanceof Buffer)*/ !Buffer.isBuffer(buffer)) throw new Error('invalid buffer')
+        if (typeof buffer === 'undefined' || /*!(buffer instanceof Buffer)*/ !Buffer.isBuffer(buffer)) throw new LogicError('invalid buffer')
 
         this._validateUserExists(username)
 
@@ -94,21 +116,13 @@ const logic = {
         this._validateUserExists(username)
 
         fs.unlinkSync(`data/${username}/files/${file}`)
-    },
-
-    updateProfile(username,password,newPassword){
-        if(password !== this._users[username].password) throw new Error(`wrong password`)
-
-        this._validateStringField('new password', newPassword)
-
-        this._users[username].password = newPassword
-
-        this._persist()
     }
-
-    
 }
 
-logic._users = JSON.parse(fs.readFileSync('data/users.json'))
+class LogicError extends Error {
+    constructor(message) {
+        super(message)
+    }
+}
 
-module.exports = logic
+module.exports = { logic, LogicError }
