d484e2b1796aa15cc16fe99febd33a974f78e5cd
sergiSanramaT <sergisanramat@gmail.com>
2018-08-17 16:23:07 +0200

    update routes api

--- staff/sergi-sanrama/files-api/logic/index.js
+++ staff/sergi-sanrama/files-api/logic/index.js
@@ -13,37 +13,43 @@ const logic = {
         if (typeof fieldValue !== 'string' || !fieldValue.length) throw new LogicError(`invalid ${fieldName}`)
     },
 
-    _validateUserExists(username) {
-        return this._users.findOne({ username })
-            .then(user => {
-                if (!user) throw new LogicError(`user ${username} does not exist`)
-            })
-    },
+    // _validateUserExists(username) {
+    //     return this._users.findOne({ username })
+    //         .then(user => {
+    //             if (!user) throw new LogicError(`user ${username} does not exist`)
+    //         })
+    // },
 
     register(username, password) {
         return Promise.resolve()
             .then(() => {
                 this._validateStringField('username', username)
                 this._validateStringField('password', password)
+
                 return this._users.findOne({ username })
             })
             .then(user => {
                 if (user) throw new LogicError(`user ${username} already exists`)
+
                 const _user = { username, password }
+
                 return this._users.insertOne(_user)
             })
             .then(() =>
                 new Promise((resolve, reject) => {
                     fs.mkdir(`data/${username}`, err => {
                         if (err) return reject(err)
+
                         fs.mkdir(`data/${username}/files`, err => {
                             if (err) return reject(err)
+
                             resolve()
                         })
                     })
                 })
             )
     },
+
 
     authenticate(username, password) {
         return Promise.resolve()
@@ -54,11 +60,12 @@ const logic = {
                 return this._users.findOne({ username })
             })
             .then(user => {
-                if (!user) throw new LogicError(`user ${username} does not exists`)
+                if (!user) throw new LogicError(`user ${username} does not exist`)
+
                 if (user.password !== password) throw new LogicError('wrong credentials')
-                
+
                 return true
-            })        
+            })
     },
 
     updatePassword(username, password, newPassword) {
@@ -67,12 +74,18 @@ const logic = {
                 this._validateStringField('username', username)
                 this._validateStringField('password', password)
                 this._validateStringField('new password', newPassword)
+
                 return this._users.findOne({ username })
             })
             .then(user => {
                 if (!user) throw new LogicError(`user ${username} does not exists`)
+
+                if (user.password !== password) throw new LogicError('wrong credentials')
+
                 if (password === newPassword) throw new LogicError('new password cannot be same as current password')
-                return this._users.updateOne({username}, { $set: { password: newPassword } })
+
+                //return this._users.updateOne({ username }, { $set: { password: newPassword } })
+                return this._users.updateOne({ _id: user._id }, { $set: { password: newPassword } })
             })
     },
 
@@ -80,8 +93,20 @@ const logic = {
         return Promise.resolve()
             .then(() => {
                 this._validateStringField('username', username)
-                this._validateUserExists(username)
-                fs.readdir(`data/${username}/files`)
+
+                return this._users.findOne({ username })
+            })
+            .then((user) => {
+                if (!user) throw new LogicError(`user ${username} does not exist`)
+
+                return new Promise((resolve, reject) => {
+                    fs.readdir(`data/${username}/files`, (err, files) => {
+                        if (err) return reject(err)
+
+                        resolve(files)
+                    })
+                })
+
             })
     },
 
@@ -90,33 +115,51 @@ const logic = {
             .then(() => {
                 this._validateStringField('username', username)
                 this._validateStringField('filename', filename)
-                if (typeof buffer === 'undefined' || !Buffer.isBuffer(buffer)) throw new LogicError('invalid buffer')
-                this._validateUserExists(username)
+
+                if (typeof buffer === 'undefined' || /*!(buffer instanceof Buffer)*/ !Buffer.isBuffer(buffer)) throw new LogicError('invalid buffer')
+
+                return this._users.findOne({ username })
             })
-            .then(() => {
-                fs.writeFile(`data/${username}/files/${filename}`, buffer)
+            .then((user) => {
+                if (!user) throw new LogicError(`user ${username} does not exist`)
+
+                return new Promise((resolve, reject) => {
+                    fs.writeFile(`data/${username}/files/${filename}`, buffer, err => {
+                        if (err) return reject(err)
+
+                        resolve(true)
+                    })
+                })
             })
     },
 
     getFilePath(username, file) {
-        return Promise.resolve()
-            .then(() => {
-                this._validateStringField('username', username)
-                this._validateStringField('file', file)
-                this._validateUserExists(username)
-                return `data/${username}/files/${file}`
-            })
+        this._validateStringField('username', username)
+        this._validateStringField('file', file)
+
+        this._validateUserExists(username)
+
+        return `data/${username}/files/${file}`
     },
 
-    removeFile(username, file) {
+    removeFile(username, filename) {
         return Promise.resolve()
             .then(() => {
                 this._validateStringField('username', username)
-                this._validateStringField('file', file)
-                this._validateUserExists(username)
+                this._validateStringField('filename', filename)
+
+                return this._users.findOne({ username })
             })
-            .then(() => {
-                fs.unlink(`data/${username}/files/${file}`)
+            .then((user) => {
+                if (!user) throw new LogicError(`user ${username} does not exist`)
+
+                return new Promise((resolve, reject) => {
+                    fs.unlink(`data/${username}/files/${filename}`, err => {
+                        if (err) return reject(err)
+
+                        resolve(true)
+                    })
+                })
             })
     }
 }
--- staff/sergi-sanrama/files-api/routes.js
+++ staff/sergi-sanrama/files-api/routes.js
@@ -14,47 +14,38 @@ const jsonBodyParser = bodyParser.json()
 router.post('/register', jsonBodyParser, (req, res) => {
     const { body: { username, password } } = req
 
-    try {
-        logic.register(username, password)
-
-        res.status(201).json({ message: 'user registered' })
-    } catch (err) {
-        const { message } = err
-
-        res.status(err instanceof LogicError? 400 : 500).json({ message })
-    }
-})
+    logic.register(username, password)
+        .then(() => res.status(201).json({ message: 'user registered' }))
+        .catch((err =>{
+            const { message } = err
+            res.status(err instanceof LogicError? 400 : 500).json({ message })
+        }) 
+)
 
 router.post('/authenticate', jsonBodyParser, (req, res) => {
     const { body: { username, password } } = req
 
-    try {
-        logic.authenticate(username, password)
-
+    logic.authenticate(username, password)
         const { JWT_SECRET, JWT_EXP } = process.env
-
         const token = jwt.sign({ sub: username }, JWT_SECRET, { expiresIn: JWT_EXP })
 
-        res.status(200).json({ message: 'user authenticated', token })
-    } catch (err) {
-        const { message } = err
-
-        res.status(err instanceof LogicError? 401 : 500).json({ message })
-    }
+        .then(() =>  res.status(200).json({ message: 'user authenticated', token }) )
+        .catch(err =>{
+            const { message } = err
+            res.status(err instanceof LogicError? 401 : 500).json({ message })
+        })
+    
 })
 
 router.patch('/user/:username', [validateJwt, jsonBodyParser], (req, res) => {
     const { params: { username }, body: { password, newPassword } } = req
 
-    try {
-        logic.updatePassword(username, password, newPassword)
-
-        res.status(200).json({ message: 'user updated' })
-    } catch (err) {
-        const { message } = err
-
-        res.status(err instanceof LogicError? 400 : 500).json({ message })
-    }
+    logic.updatePassword(username, password, newPassword)
+        .then(() => res.status(200).json({ message: 'user updated' }) )
+        .catch(err => {
+            const { message } = err
+            res.status(err instanceof LogicError? 400 : 500).json({ message })    
+        })
 })
 
 router.get('/user/:username/files', validateJwt, (req, res) => {
@@ -75,42 +66,33 @@ router.post('/user/:username/files', [validateJwt, fileUpload()], (req, res) => 
     const { params: { username }, files: { upload } } = req
 
     if (upload) {
-        try {
-            logic.saveFile(username, upload.name, upload.data)
-
-            res.status(201).json({ message: 'file saved' })
-        } catch (err) {
-            const { message } = err
-    
-            res.status(err instanceof LogicError? 400 : 500).json({ message })
-        }
+        logic.saveFile(username, upload.name, upload.data)
+            .then(() => res.status(201).json({ message: 'file saved' }))
+            .catch(err =>{
+                const { message } = err    
+                res.status(err instanceof LogicError? 400 : 500).json({ message })
+        })
     } else
         res.status(418).json({ message: 'no file received' })
-
 })
 
 router.get('/user/:username/files/:file', validateJwt, (req, res) => {
     const { params: { username, file } } = req
-
-    try {
-        res.download(logic.getFilePath(username, file))
-    } catch ({ message }) {
-        res.status(500).json({ message })
-    }
+        .then(() => res.download(logic.getFilePath(username, file)))
+        .catch({ message }) 
+            res.status(500).json({ message })
+        })
 })
 
 router.delete('/user/:username/files/:file', validateJwt, (req, res) => {
     const { params: { username, file } } = req
 
-    try {
-        logic.removeFile(username, file)
-
-        res.status(200).json({ message: 'file deleted' })
-    } catch (err) {
-        const { message } = err
-
-        res.status(err instanceof LogicError? 400 : 500).json({ message })
-    }
+    logic.removeFile(username, file)
+        .then(() =>res.status(200).json({ message: 'file deleted' }) )
+        .catch(err =>{
+            const { message } = err
+            res.status(err instanceof LogicError? 400 : 500).json({ message })
+    })
 })
 
 module.exports = function(db) {
