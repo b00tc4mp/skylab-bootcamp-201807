1b443e84599e7637f0b8b06bbfb2b345b52b2189
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-09-04 15:40:36 +0200

    unified

--- staff/giorgio-delabarrera/inskygram/server/src/logic/index.ts
+++ staff/giorgio-delabarrera/inskygram/server/src/logic/index.ts
@@ -1,6 +1,286 @@
-import user from "./user";
-import post from "./post";
+import validator from "validator";
+import User, { UserModelInterface } from "../../models/user";
+import LogicError from "../error/logic-error";
+import cloudinary from "../../config/cloudinary";
 
-const logic = { user, post };
+const logic = {
+
+  register(username: string, email: string, password: string): Promise<boolean> | never {
+
+    return Promise.resolve()
+      .then(() => {
+        if (!username) { throw new LogicError("invalid username"); }
+        if (!email) { throw new LogicError("invalid email"); }
+        if (!validator.isEmail(email)) { throw new LogicError("invalid email"); }
+        if (!password) { throw new LogicError("invalid password"); }
+
+        return User.findOne({ username });
+      })
+      .then((user: UserModelInterface) => {
+        if (user) { throw new LogicError(`user with username ${username} already exists`); }
+
+        return User.findOne({ email });
+      })
+      .then((user: UserModelInterface) => {
+        if (user) { throw new LogicError(`user with email ${email} already exists`); }
+
+        return User.findOne({ email });
+      })
+      .then(() => User.create({ username, email, password }))
+      .then(() => true);
+  },
+
+  authenticate(username: string, password: string): Promise<boolean> | never {
+    return Promise.resolve()
+      .then(() => {
+
+        if (!username) { throw new LogicError("invalid username"); }
+        if (!password) { throw new LogicError("invalid password"); }
+
+        return User.findOne({ username });
+      })
+      .then((user: UserModelInterface) => {
+        if (!user) { throw new LogicError(`user with username ${username} does not exists`); }
+
+        if (user.password !== password) { throw new LogicError(`wrong password`); }
+
+        user.lastLogin = new Date();
+
+        return user.save();
+      })
+      .then((user: UserModelInterface) => true);
+  },
+
+  retrieveUser(username?: string, targetUsername: string): Promise<UserModelInterface> | never {
+    return Promise.resolve()
+      .then(() => {
+        if (!username) { throw new LogicError("invalid username"); }
+
+        return User.findOne({ username }, { password: 0, __v: 0 });
+      })
+      .then((user: UserModelInterface) => user);
+  },
+
+  followUser(username: string, targetUsername: string) {},
+
+  listUserFollowers(username?: string, targetUsername: string) {},
+
+  listUserFollowings(username?: string, targetUsername: string) {},
+
+  listUserPosts(username?: string, targetUsername: string) {},
+
+  listUserPostsSaved(username?: string, targetUsername: string) {},
+
+  updateUser(
+    username: string,
+    newEmail?: string,
+    name?: string,
+    website?: string,
+    phoneNumber?: string,
+    gender?: string,
+    biography?: string,
+    privateAccount?: boolean,
+  ): Promise<boolean> | never {
+    return Promise.resolve()
+      .then(() => {
+        if (!username) { throw new LogicError("invalid username"); }
+
+        return User.findOne({ username });
+      })
+      .then((user: UserModelInterface) => {
+        if (!user) { throw new LogicError(`user with username ${username} does not exists`); }
+
+        if (newEmail) {
+          return new Promise((resolve, reject) => {
+            User.findOne({ email: newEmail })
+              .then((foundUser: UserModelInterface) => {
+                if (foundUser) { reject(new LogicError(`user with email ${newEmail} already exists`)); }
+
+                user.email = newEmail;
+
+                resolve(user);
+              });
+          });
+        }
+        return user;
+      })
+      .then((user: UserModelInterface) => {
+        if (name) { user.name = name; }
+        if (website) { user.website = website; }
+        if (phoneNumber) { user.phoneNumber = phoneNumber; }
+        if (gender) { user.gender = gender; }
+        if (biography) { user.biography = biography; }
+        if (privateAccount !== undefined) { user.privateAccount = privateAccount; }
+
+        return user.save();
+      })
+      .then(() => true);
+  },
+
+  updateUserPassword(username: string, password: string, newPassword: string): Promise<boolean> | never {
+    return Promise.resolve()
+      .then(() => {
+        if (!username) { throw new LogicError("invalid username"); }
+        if (!password) { throw new LogicError("invalid password"); }
+        if (!newPassword) { throw new LogicError("invalid new password"); }
+
+        return User.findOne({ username });
+      })
+      .then((user: UserModelInterface) => {
+        if (!user) { throw new LogicError(`user with email ${username} does not exists`); }
+
+        if (user.password !== password) { throw new LogicError(`wrong password`); }
+
+        if (password === newPassword) { throw new LogicError("new password must be different to old password"); }
+
+        user.password = newPassword;
+
+        return user.save();
+      })
+      .then(() => true);
+  },
+
+  updateUserAvatar(username: string, filename: string, buffer: Buffer): Promise<boolean> | never {
+    return Promise.resolve()
+      .then(() => {
+        if (!username) { throw new LogicError("invalid username"); }
+        if (!filename) { throw new LogicError("invalid filename"); }
+        if (!buffer) { throw new LogicError("invalid buffer"); }
+
+        return User.findOne({ username });
+      })
+      .then((user: UserModelInterface) => {
+        if (!user) { throw new LogicError(`user with username ${username} does not exists`); }
+
+        return new Promise((resolve, reject) => {
+          cloudinary.v2.uploader.upload_stream((error: any, result: any) => {
+            if (error) {
+              return reject(new LogicError(`the file ${filename} could not be uploaded`));
+            }
+
+            user.imageId = result.public_id;
+
+            resolve(user.save());
+
+          }).end(buffer);
+        });
+      })
+      .then((user: UserModelInterface) => true);
+  },
+
+  listUserWall(username: string, limit: number = 10, offset: number = 1) {},
+
+  createPost(username: string, filename: string, buffer: Buffer, caption?: string): Promise<string> | never {
+    return Promise.resolve()
+      .then(() => {
+        if (!filename) { throw new LogicError("invalid filename"); }
+        if (!buffer) { throw new LogicError("invalid buffer"); }
+
+        return User.findOne({ username });
+      })
+      .then((user: UserModelInterface) => {
+        if (!user) { throw new LogicError(`user with username ${username} does not exists`); }
+
+        return new Promise((resolve, reject) => {
+          cloudinary.v2.uploader.upload_stream((error: any, result: any) => {
+            if (error) {
+              return reject(new LogicError(`the file ${filename} could not be uploaded`));
+            }
+
+            const post = new Post();
+
+            post.imageId = result.public_id;
+            post.user = user._id;
+            if (caption) { post.caption = caption; }
+
+            resolve(post.save());
+
+          }).end(buffer);
+        });
+      })
+      .then((post: PostModelInterface) => post.id);
+  },
+
+  retrievePost(username: string, postId: string): Promise<PostModelInterface> | never {
+    let post: PostModelInterface;
+    return Promise.resolve()
+      .then(() => {
+        if (!postId) { throw new LogicError("invalid post id"); }
+
+        return Post.findOne({ _id: postId }, { __v: 0 });
+      })
+      .then((foundPost: PostModelInterface) => {
+        if (!foundPost) { throw new LogicError(`post with id ${postId} does not exists`); }
+
+        post = foundPost;
+
+        return User.findById(post.user);
+      })
+      .then((user: UserModelInterface) => {
+        if (!user) { throw new LogicError(`user with id ${post.user} does not exists`); }
+
+        // if (user.privateAccount)
+
+        return Post.findOne({ _id: postId, user: user._id }, { __v: 0 });
+      });
+
+    // .then((user: UserModelInterface) => {
+    //   if (!user) { throw new LogicError(`user with username ${username} does not exists`); }
+
+    //   return Post.findOne({ _id: id, user: user._id }, { __v: 0 });
+    // })
+    // .then((post: PostModelInterface) => {
+    //   if (!post) { throw new LogicError(`post with id ${id} does not exists`); }
+
+    //   return post;
+    // });
+  },
+
+  addCommentToPost(username: string, postId: string, description: string): Promise<boolean> | never {
+    let post: PostModelInterface;
+
+    return Promise.resolve()
+      .then(() => {
+        if (!username) { throw new LogicError("invalid username"); }
+        if (!postId) { throw new LogicError("invalid post id"); }
+        if (!commentUsername) { throw new LogicError("invalid comment username"); }
+        if (!description) { throw new LogicError("invalid comment description"); }
+
+        return User.findOne({ username });
+      })
+      .then((user: UserModelInterface) => {
+        if (!user) { throw new LogicError(`user with username ${username} does not exists`); }
+
+        return Post.findOne({ _id: postId, user: user._id });
+      })
+      .then((foundPost: PostModelInterface) => {
+        if (!foundPost) { throw new LogicError(`post with id ${postId} does not exists`); }
+
+        post = foundPost;
+
+        return User.findOne({ commentUsername });
+      })
+      .then((commentUser: UserModelInterface) => {
+        if (!commentUser) { throw new LogicError(`comment user with username ${username} does not exists`); }
+
+        const comment = new Comment();
+        comment.description = description;
+        comment.user = commentUser._id;
+
+        post.comments.push(comment);
+
+        return post.save();
+      })
+      .then((post: PostModelInterface) => true);
+  },
+
+  toggleLikePost(username: string, postId: string) {},
+
+  toggleSavePost(username: string, postId: string) {},
+
+  listPopularPublicPosts() {},
+
+  search(query: string) {},
+};
 
 export default logic;
